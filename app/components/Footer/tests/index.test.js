7a0544d28e6f417c8f0da8245496c744f548219f
Sergey Ilin <ilinserg@gmail.com>
2018-09-20 22:30:07 -0500

    Fix for unit tests

--- app/components/Footer/tests/index.test.js
+++ app/components/Footer/tests/index.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 import renderer from 'react-test-renderer';
 import 'jest-styled-components';
-
 import Footer from '../index';
 
 describe('<Footer />', () => {
   it('should match the snapshot', () => {
-    const renderedComponent = renderer.create(<Footer />).toJSON();
+    const renderedComponent = renderer
+      .create(
+        <BrowserRouter>
+          <IntlProvider locale="en">
+            <Footer />
+          </IntlProvider>
+        </BrowserRouter>,
+      )
+      .toJSON();
     expect(renderedComponent).toMatchSnapshot();
   });
 });
--- app/components/Header/tests/index.test.js
+++ app/components/Header/tests/index.test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
@@ -6,7 +8,15 @@ import Header from '../index';
 
 describe('<Header />', () => {
   it('should match the snapshot', () => {
-    const renderedComponent = renderer.create(<Header />).toJSON();
+    const renderedComponent = renderer
+      .create(
+        <BrowserRouter>
+          <IntlProvider locale="en">
+            <Header />
+          </IntlProvider>
+        </BrowserRouter>,
+      )
+      .toJSON();
     expect(renderedComponent).toMatchSnapshot();
   });
 });
