ff1020007077b29c994839e56a58eba6b76198de
Roman Remniov <remkst777@rambler.ru>
2018-11-05 21:32:37 +0300

    Minimized TextEditor functionality

--- app/components/TextEditor/TextEditorConfig.js
+++ app/components/TextEditor/TextEditorConfig.js
@@ -2,9 +2,9 @@ const TextEditorConfig = {
   apiKey: 'fvry0is6hvuc3ij671v897r0t3854qvo6ne0k5eun4378zxk',
   menubar: false,
   plugins: [
-    'codesample textpattern advlist autolink lists link image charmap print preview anchor textcolor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table contextmenu paste code help wordcount',
+    'codesample textpattern advlist autolink lists link image preview textcolor',
+    'searchreplace visualblocks code',
+    'media contextmenu paste code help wordcount',
   ],
   toolbar:
     'insert | undo redo | codesample formatselect | bold italic backcolor  | bullist numlist | help',
