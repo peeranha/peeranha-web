02755727f09b5533b569b4a0bae0677651ec4800
Roman Remniov <remkst777@rambler.ru>
2018-11-13 13:13:19 +0300

    Added FontAwesome

---
+++ app/icons-config.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faChevronUp);
+library.add(faChevronDown);
