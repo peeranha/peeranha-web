72a408a7bcddff5b21650d6f307fc4f2dd4ef2a9
Roman Remniov <remkst777@rambler.ru>
2018-11-13 12:59:39 +0300

    updated tests

--- app/components/TextEditor/tests/index.test.js
+++ app/components/TextEditor/tests/index.test.js
@@ -1,27 +1,37 @@
 import TextEditor from '../index';
 
-jest.mock('react-simplemde-editor');
-jest.mock('simplemde/dist/simplemde.min.css');
-
-const props = {
+const cmp = new TextEditor();
+cmp.props = {
   input: {},
   content: 'content',
   height: 400,
   handleEditorChange: jest.fn(),
+  onChange: jest.fn(),
 };
 
+jest.mock('react-simplemde-editor');
+jest.mock('simplemde/dist/simplemde.min.css');
+
 describe('<TextEditor />', () => {
+  describe('handleEditorChange', () => {
+    it('test', () => {
+      const txt = 'some txt';
+      cmp.handleEditorChange(txt);
+      expect(cmp.props.onChange).toHaveBeenCalledWith(txt);
+    });
+  });
+
   describe('snapshot test', () => {
     it('@content is null, @height is null', () => {
-      props.content = null;
-      props.height = null;
-      expect(TextEditor(props)).toMatchSnapshot();
+      cmp.props.content = null;
+      cmp.props.height = null;
+      expect(cmp.render()).toMatchSnapshot();
     });
 
     it('@content is not null, @height is not null', () => {
-      props.content = 'content';
-      props.height = 400;
-      expect(TextEditor(props)).toMatchSnapshot();
+      cmp.props.content = 'content';
+      cmp.props.height = 400;
+      expect(cmp.render()).toMatchSnapshot();
     });
   });
 });
