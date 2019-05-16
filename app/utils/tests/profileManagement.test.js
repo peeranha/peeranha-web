7d0a4654be85b4db4dd23308f3744c0b6e878fe7
Roman <remkst777@rambler.ru>
2019-05-16 18:05:54 +0300

    fetcher

--- app/utils/tests/profileManagement.test.js
+++ app/utils/tests/profileManagement.test.js
@@ -1,12 +1,11 @@
-import EosioService from '../eosio';
-
 import { saveFile, getFileUrl, getText, saveText } from '../ipfs';
 
 import {
   uploadImg,
   getProfileInfo,
   saveProfile,
-  getCitiesList,
+  UsersFetcher,
+  AccountsSortedBy,
 } from '../profileManagement';
 
 import { ACCOUNT_TABLE, ALL_ACCOUNTS_SCOPE } from '../constants';
@@ -20,12 +19,59 @@ jest.mock('../ipfs', () => ({
 
 jest.setTimeout(10000);
 
-const cmp = new EosioService();
-cmp.sendTransaction = jest.fn().mockImplementation(() => {});
-cmp.getTableRow = jest.fn().mockImplementation(() => {});
+const cmp = {
+  sendTransaction: jest.fn(),
+  getTableRow: jest.fn(),
+  getTableRows: jest.fn(),
+};
 
 beforeEach(() => {
   jest.clearAllMocks();
+});
+
+describe('Fetcher', () => {
+  const firstFetchCount = 10;
+  const fetchCount = 10;
+  const sortType = AccountsSortedBy.rating;
+
+  const fetcherEx = new UsersFetcher(
+    firstFetchCount,
+    fetchCount,
+    sortType,
+    cmp,
+  );
+
+  it('constructor', () => {
+    fetcherEx.constructor(firstFetchCount, fetchCount, sortType, cmp);
+
+    expect(fetcherEx.sortType).toBe(sortType);
+    expect(fetcherEx.firstFetchCount).toBe(firstFetchCount);
+    expect(fetcherEx.fetchCount).toBe(fetchCount);
+    expect(fetcherEx.itemArray).toEqual([]);
+    expect(fetcherEx.hasMoreToFetch).toBe(true);
+    expect(fetcherEx.eosService).toEqual(cmp);
+  });
+
+  it('getNextItems', async () => {
+    fetcherEx.eosService.getTableRows.mockImplementation(() => []);
+
+    const res = await fetcherEx.getNextItems();
+
+    expect(res).not.toBe(undefined);
+  });
+});
+
+it('AccountsSortedBy', () => {
+  expect(typeof AccountsSortedBy.rating).toBe('object');
+  expect(typeof AccountsSortedBy.registration_time).toBe('object');
+});
+
+it('UsersFetcher', () => {
+  const fetcher = new UsersFetcher();
+
+  expect(fetcher.TABLE).toBe(ACCOUNT_TABLE);
+  expect(fetcher.SCOPE).toBe(ALL_ACCOUNTS_SCOPE);
+  expect(fetcher.PRIMARY_KEY).toBe('user');
 });
 
 /* eslint camelcase: 0 */
@@ -84,13 +130,4 @@ it('saveProfile', async () => {
 
   expect(saveText).toHaveBeenCalledWith(JSON.stringify(profile));
   expect(cmp.sendTransaction).toHaveBeenCalledTimes(1);
-});
-
-it('getCitiesList', async () => {
-  const city = 'Chicago';
-  const citiesList = await getCitiesList(city);
-
-  expect(!!citiesList[0]).toBe(true);
-  expect(citiesList[0].name).toBe(city);
-  expect(citiesList.length).toBe(5);
 });
