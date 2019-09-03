2d36c6b8c97d4cb554a250b5ad259b30e5205a47
Roman <remkst777@rambler.ru>
2019-09-03 19:44:41 +0300

    added support page

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -6,27 +6,44 @@ import { makeSelectLocale } from 'containers/LanguageProvider/selectors';
 
 import { sendEmail, sendMessage } from 'utils/homepageManagement';
 
-import { SEND_EMAIL, SEND_MESSAGE } from './constants';
+import {
+  SEND_EMAIL,
+  SEND_MESSAGE,
+  EMAIL_FIELD,
+  REFCODE_FIELD,
+  NAME_FIELD,
+  SUBJECT_FIELD,
+  MESSAGE_FIELD,
+} from './constants';
 
 import {
   sendEmailSuccess,
   sendEmailErr,
   sendMessageSuccess,
   sendMessageErr,
-  closeHeaderPopup,
 } from './actions';
 
 import messages from './messages';
 
-export function* sendEmailWorker({ formData, reset, pageInfo }) {
+export function* sendEmailWorker({ val }) {
   const locale = yield select(makeSelectLocale());
 
   try {
+    const { reset, form } = val[2];
+
+    const formData = {
+      email: val[0].get(EMAIL_FIELD),
+      refCode: val[0].get(REFCODE_FIELD),
+    };
+
+    const pageInfo = {
+      url: window.location.href,
+      name: `${translationMessages[locale][messages.title.id]}, ${form}`,
+    };
+
     yield call(() => sendEmail(formData, pageInfo));
 
-    yield call(() => reset());
-
-    yield put(closeHeaderPopup());
+    yield call(reset);
 
     yield put(
       addToast({
@@ -49,13 +66,32 @@ export function* sendEmailWorker({ formData, reset, pageInfo }) {
   }
 }
 
-export function* sendMessageWorker({ formData, reset, pageInfo }) {
+export function* sendMessageWorker({ val }) {
   const locale = yield select(makeSelectLocale());
 
   try {
+    const { reset, form } = val[2];
+
+    const subject =
+      typeof val[0].get(SUBJECT_FIELD) === 'object'
+        ? val[0].get(SUBJECT_FIELD).value
+        : val[0].get(SUBJECT_FIELD);
+
+    const formData = {
+      email: val[0].get(EMAIL_FIELD),
+      firstname: val[0].get(NAME_FIELD),
+      subject,
+      message: val[0].get(MESSAGE_FIELD),
+    };
+
+    const pageInfo = {
+      url: window.location.href,
+      name: `${translationMessages[locale][messages.title.id]}, ${form}`,
+    };
+
     yield call(() => sendMessage(formData, pageInfo));
 
-    yield call(() => reset());
+    yield call(reset);
 
     yield put(
       addToast({
---
+++ app/containers/Support/FaqSections.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import * as routes from 'routes-config';
+import commonMessages from 'common-messages';
+import { TEXT_PRIMARY } from 'style-constants';
+
+import { getSectionCode } from 'utils/faqManagement';
+
+import A from 'components/A';
+import { BaseStyled } from 'components/TextEditor/Tips';
+
+const Base = BaseStyled.extend`
+  h6 {
+    text-transform: uppercase;
+    font-weight: bold;
+    margin-top: 15px;
+    margin-bottom: 20px;
+  }
+
+  li {
+    margin-bottom: 10px;
+
+    ${A} {
+      color: ${TEXT_PRIMARY};
+    }
+  }
+`;
+
+const FaqSections = ({ faq }) => (
+  <Base>
+    <h6>
+      <FormattedMessage {...commonMessages.faq} />
+    </h6>
+
+    {faq && (
+      <ul>
+        {faq.blocks.map((x, sectionIndex) => (
+          <li>
+            <A to={routes.appFaq(getSectionCode(sectionIndex))}>{x.h2}</A>
+          </li>
+        ))}
+      </ul>
+    )}
+  </Base>
+);
+
+FaqSections.propTypes = {
+  faq: PropTypes.array,
+};
+
+export default React.memo(FaqSections);
