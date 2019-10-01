f952fed847b9dd00e905425161ca9020c34e81d2
remkst777 <remkst777@rambler.ru>
2019-10-01 18:02:04 +0300

    fix

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -103,7 +103,8 @@ module.exports = options => {
               loader: 'image-webpack-loader',
               options: {
                 mozjpeg: {
-                  enabled: false,
+                  enabled: true,
+                  progressive: true,
                   // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
                   // Try enabling it in your environment by switching the config to:
                   // enabled: true,
@@ -116,7 +117,7 @@ module.exports = options => {
                   optimizationLevel: 7,
                 },
                 pngquant: {
-                  quality: '65-90',
+                  quality: [0.7, 0.8],
                   speed: 4,
                 },
               },
