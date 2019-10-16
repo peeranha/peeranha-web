51623a06d7f66f3aa33f0bd9260ee513832cb586
remkst777 <31829395+remkst777@users.noreply.github.com>
2019-10-16 14:59:11 +0300

    gateway and api for ipfs ports (#84)
    * gateway and api for ipfs ports
    
    * protocol fix

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,8 +2,6 @@ export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
 
-export const IPFS_URL = 'http://localhost:8080/ipfs';
-
 export const BLOCKCHAIN_NAME = 'eos';
 export const DEFAULT_EOS_PERMISSION = 'active';
 export const SCATTER_APP_NAME = 'Peeranha';
