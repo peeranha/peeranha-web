d30ada3767d91cfd33749d4b9d505ef1b66deb17
remkst777 <remkst777@rambler.ru>
2019-11-18 15:24:37 +0300

    tests

--- app/containers/AccountProvider/tests/saga.test.js
+++ app/containers/AccountProvider/tests/saga.test.js
@@ -3,15 +3,12 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
-import { select, all, call } from 'redux-saga/effects';
+import { select, call } from 'redux-saga/effects';
 
 import { getProfileInfo } from 'utils/profileManagement';
 import { getBalance } from 'utils/walletManagement';
 
-import {
-  GET_USER_PROFILE,
-  GET_USER_PROFILE_SUCCESS,
-} from 'containers/DataCacheProvider/constants';
+import { GET_USER_PROFILE } from 'containers/DataCacheProvider/constants';
 
 import { FOLLOW_HANDLER_SUCCESS } from 'containers/FollowCommunityButton/constants';
 import { SHOW_SCATTER_SIGNUP_FORM_SUCCESS } from 'containers/SignUp/constants';
@@ -35,6 +32,8 @@ import { EDIT_ANSWER_SUCCESS } from 'containers/EditAnswer/constants';
 import { EDIT_QUESTION_SUCCESS } from 'containers/EditQuestion/constants';
 import { SEND_TOKENS_SUCCESS } from 'containers/SendTokens/constants';
 import { PICKUP_REWARD_SUCCESS } from 'containers/Wallet/constants';
+import { PROFILE_INFO_LS } from 'containers/Login/constants';
+import { getUserProfileSuccess } from 'containers/DataCacheProvider/actions';
 
 import {
   DELETE_ANSWER_SUCCESS,
@@ -58,8 +57,6 @@ import defaultSaga, { getCurrentAccountWorker } from '../saga';
 import { GET_CURRENT_ACCOUNT, GET_CURRENT_ACCOUNT_ERROR } from '../constants';
 
 import { getCurrentAccountSuccess } from '../actions';
-import { PROFILE_INFO_LS } from 'containers/Login/constants';
-import { getUserProfileSuccess } from 'containers/DataCacheProvider/actions';
 
 jest.mock('redux-saga/effects', () => ({
   select: jest.fn().mockImplementation(() => {}),
@@ -83,6 +80,14 @@ const localStorage = {
 };
 
 Object.defineProperty(global, 'localStorage', { value: localStorage });
+
+beforeEach(() => {
+  call.mockClear();
+  getProfileInfo.mockClear();
+  getBalance.mockClear();
+  localStorage.getItem.mockClear();
+  localStorage.setItem.mockClear();
+});
 
 describe('getCurrentAccountWorker', () => {
   describe('eos service was initialized with account', () => {
@@ -171,7 +176,7 @@ describe('getCurrentAccountWorker', () => {
 
   describe('eos service was NOT initialized with account and there is no @initAccount', () => {
     const account = null;
-    const prevProfileInfo = null;
+    const prevProfileInfo = { profile: {} };
 
     const autoLoginData = {
       eosAccountName: 'eosAccountName111',
@@ -192,17 +197,138 @@ describe('getCurrentAccountWorker', () => {
     generator.next();
     generator.next(eos);
     generator.next(prevProfileInfo);
+
+    it('get eosName from localStorage', () => {
+      localStorage.getItem.mockImplementationOnce(() =>
+        JSON.stringify(autoLoginData),
+      );
+
+      generator.next(account);
+      generator.next([profileInfo, balance]);
+
+      expect(profileInfo).toEqual({
+        ...profileInfo,
+        profile: prevProfileInfo.profile,
+        balance,
+      });
+    });
+  });
+
+  describe('@initAccount was passed', () => {
+    const account = 'account';
+    const prevProfileInfo = { profile: {} };
+
+    const profileInfo = {};
+    const balance = 10;
+
+    const eos = {
+      scatterInstalled: true,
+      initialized: true,
+      getSelectedAccount: () => account,
+    };
+
+    const generator = getCurrentAccountWorker(account);
+
+    generator.next();
+    generator.next();
+    generator.next(eos);
+    generator.next(prevProfileInfo);
     generator.next(account);
+    generator.next([profileInfo, balance]);
 
-    it('test', () => {});
+    it('getCurrentAccountSuccess with @account', () => {
+      const step = generator.next();
+      expect(step.value).toEqual(getCurrentAccountSuccess(account, balance));
+    });
   });
 
-  describe('@initAccount was passed', () => {});
   describe('@prevProfileInfo is null', () => {
-    describe('userProfileLS is same as current account', () => {});
-    describe('userProfileLS is NOT same as current account', () => {});
+    describe('userProfileLS is same as current account', () => {
+      const account = 'account';
+      const prevProfileInfo = null;
+
+      const profileInfoLS = { user: account, balance: 10 };
+
+      const eos = {
+        scatterInstalled: true,
+        initialized: true,
+        getSelectedAccount: () => account,
+      };
+
+      const generator = getCurrentAccountWorker(account);
+
+      generator.next();
+      generator.next();
+      generator.next(eos);
+      generator.next(prevProfileInfo);
+
+      it('getUserProfileSuccess', () => {
+        localStorage.getItem.mockImplementationOnce(() =>
+          JSON.stringify(profileInfoLS),
+        );
+
+        const step = generator.next(account);
+        expect(step.value).toEqual(getUserProfileSuccess(profileInfoLS));
+      });
+
+      it('getCurrentAccountSuccess', () => {
+        const step = generator.next();
+        expect(step.value).toEqual(
+          getCurrentAccountSuccess(profileInfoLS.user, profileInfoLS.balance),
+        );
+      });
+
+      it('done is TRUE', () => {
+        const step = generator.next();
+        expect(step.done).toBe(true);
+      });
+    });
+
+    describe('userProfileLS is NOT same as current account', () => {
+      const account = 'account';
+      const prevProfileInfo = null;
+
+      const profileInfoLS = { user: `another-${account}`, balance: 10 };
+
+      const profileInfo = {};
+      const balance = 10;
+
+      const eos = {
+        scatterInstalled: true,
+        initialized: true,
+        getSelectedAccount: () => null,
+      };
+
+      const generator = getCurrentAccountWorker(account);
+
+      generator.next();
+      generator.next();
+      generator.next(eos);
+      generator.next(prevProfileInfo);
+
+      it('current account is not equal to local storage profile', () => {
+        getProfileInfo.mockImplementationOnce(() => profileInfo);
+        getBalance.mockImplementationOnce(() => balance);
+
+        localStorage.getItem.mockImplementationOnce(() =>
+          JSON.stringify(profileInfoLS),
+        );
+
+        const step = generator.next(account);
+        expect(step.value).toEqual([profileInfo, balance]);
+      });
+
+      it('getUserProfileSuccess', () => {
+        const step = generator.next([profileInfo, balance]);
+        expect(step.value).toEqual(getUserProfileSuccess(profileInfo));
+      });
+
+      it('getCurrentAccountSuccess', () => {
+        const step = generator.next();
+        expect(step.value).toEqual(getCurrentAccountSuccess(account, balance));
+      });
+    });
   });
-  describe('@prevProfileInfo is NOT null', () => {});
 });
 
 describe('defaultSaga', () => {
