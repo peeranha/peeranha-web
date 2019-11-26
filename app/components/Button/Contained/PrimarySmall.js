9801a2d015da6eea47dbe3cd5536a43b094602f1
remkst777 <remkst777@rambler.ru>
2019-11-26 13:40:19 +0300

    labels for best answer

---
+++ app/components/Button/Contained/PrimarySmall.js
@@ -0,0 +1,6 @@
+import Primary from './Primary';
+
+export default Primary.extend`
+  padding: 4px 8px;
+  font-size: 14px;
+`;
--- app/containers/ViewQuestion/BestAnswerMarker.jsx
+++ app/containers/ViewQuestion/BestAnswerMarker.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { BG_LIGHT, BORDER_SECONDARY, BG_PRIMARY } from 'style-constants';
+
+import { BG_PRIMARY } from 'style-constants';
 
 import crownIcon from 'images/crownIcon.svg?inline';
-
-import Button from 'components/Button';
+import CommunityChoiceButton from 'components/Button/Contained/PrimarySmall';
 
 import MarkAsAcceptedIcon from './MarkAsAcceptedIcon';
 import { MARK_AS_BUTTON } from './constants';
 import messages from './messages';
-
-/* eslint indent: 0 */
-export const ButtonStyled = Button.extend`
-  background: ${x => x.bg};
-  font-size: 14px;
-  height: 22px;
-  min-width: 0;
-  padding: 0 4px;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid ${x => (x.bg === BG_LIGHT ? BORDER_SECONDARY : x.bg)};
-`;
 
 export const BestAnswerMarker = ({
   answerId,
@@ -50,12 +37,10 @@ export const BestAnswerMarker = ({
       />
 
       {isTheLargestRating ? (
-        <div className="ml-2">
-          <ButtonStyled bg={BG_PRIMARY} className="px-2">
-            <img className="d-inline-flex mr-2" src={crownIcon} alt="icon" />
-            <FormattedMessage {...messages.communityChoice} />
-          </ButtonStyled>
-        </div>
+        <CommunityChoiceButton bg={BG_PRIMARY}>
+          <img className="d-inline-flex mr-2" src={crownIcon} alt="icon" />
+          <FormattedMessage {...messages.communityChoice} />
+        </CommunityChoiceButton>
       ) : null}
     </div>
   );
