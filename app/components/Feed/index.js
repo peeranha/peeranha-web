bd6ca11b8257d4dbea662aa3d6af2a65bd9960b6
GoodPassiveMan <32172971+GoodPassiveMan@users.noreply.github.com>
2020-02-10 18:37:25 +0300

    prohibit feed view for unauthenticated users

--- app/components/Feed/index.js
+++ app/components/Feed/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import * as routes from 'routes-config';
 import Questions from 'containers/Questions';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 
-/* eslint react/prop-types: 0 */
-export default ({ match }) => (
-  <Questions parentPage={routes.feed()} match={match} />
-);
+import { NotFoundPage } from 'containers/App/imports';
+import { makeSelectAccount } from 'containers/AccountProvider/selectors';
+
+const Feed = ({ match, account }) =>
+  account ? (
+    <Questions parentPage={routes.feed()} match={match} />
+  ) : (
+    <NotFoundPage />
+  );
+
+Feed.propTypes = {
+  match: PropTypes.object,
+  account: PropTypes.string,
+};
+
+export default compose(
+  connect(state => ({
+    account: makeSelectAccount()(state),
+  })),
+)(Feed);
