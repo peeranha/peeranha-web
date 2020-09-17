95b55749a675c3905405b3be0e524985116ae789
Sergey Kashko <kashko.s.a@yandex.by>
2020-09-17 13:52:04 +0300

    feature/peer-701 icon for communities with single community mode (#255)
    * border for communities with single community mode removed
    * icon for communities with single community mode added

---
+++ app/components/ExistingCommunities/SingleCommunityIcon.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { translationMessages } from 'i18n';
+
+import singleCommunity from 'images/singleCommunity.svg?external';
+import { showPopover, closePopover } from 'utils/popover';
+
+import Icon from 'components/Icon';
+
+import messages from './messages';
+
+const SCIcon = styled(Icon)`
+  margin-left: 8px;
+  margin-right: 8px;
+`;
+
+const SCButton = styled.button`
+  display: inline;
+  position: absolute;
+  z-index: 1000;
+
+  @media only screen and (max-width: 991px) {
+    display: none;
+  }
+`;
+
+const SCButtonMob = styled(SCButton)`
+  display: none;
+
+  @media only screen and (max-width: 991px) {
+    display: inline;
+  }
+`;
+
+const SingleCommunityIcon = ({ locale, id }) => {
+  const tooltipText =
+    translationMessages[locale][messages.singleCommunityTooltip.id];
+
+  const idMobile = `${id}mob`;
+
+  const [opened, setOpened] = useState(false);
+  const setOpenedFalse = () => setOpened(false);
+
+  const iPad = () => window.navigator.userAgent.includes('iPad');
+
+  const showTooltip = () => {
+    if (!iPad()) showPopover(id, tooltipText, { timer: false });
+  };
+
+  const hideTooltip = () => {
+    if (!iPad()) closePopover(id);
+  };
+
+  const toggleTooltipMobile = (e, iconId = idMobile) => {
+    e.preventDefault();
+    if (!opened) {
+      setOpened(true);
+      showPopover(iconId, tooltipText, {
+        callback: setOpenedFalse,
+        timeout: 3500,
+        position: 'top',
+      });
+    } else {
+      closePopover(iconId, setOpenedFalse);
+    }
+  };
+
+  const iPadClick = e => {
+    if (iPad()) {
+      toggleTooltipMobile(e, id);
+    }
+  };
+
+  const onBlur = () => {
+    if (opened) {
+      closePopover(idMobile, setOpenedFalse);
+    }
+    if (opened && iPad) {
+      closePopover(id, setOpenedFalse);
+    }
+  };
+
+  return (
+    <>
+      <SCButton
+        id={id}
+        onMouseEnter={showTooltip}
+        onMouseLeave={hideTooltip}
+        onClick={e => iPadClick(e)}
+        onBlur={onBlur}
+      >
+        <SCIcon icon={singleCommunity} width="14" height="14" />
+      </SCButton>
+      <SCButtonMob
+        onBlur={onBlur}
+        id={idMobile}
+        onClick={e => toggleTooltipMobile(e)}
+      >
+        <SCIcon icon={singleCommunity} width="14" height="14" />
+      </SCButtonMob>
+    </>
+  );
+};
+
+SingleCommunityIcon.propTypes = {
+  locale: PropTypes.string,
+  id: PropTypes.number,
+};
+
+export default SingleCommunityIcon;
--- app/utils/popover.js
+++ app/utils/popover.js
@@ -1,16 +1,60 @@
-export const showPopover = (elemId, message) => {
+let timerArray = [];
+
+const setNewCurrentElement = (elemId, timerId) =>
+  timerArray.push({ elemId, timerId });
+
+const setPopover = (elemId, message, position) => {
   window.$(`#${elemId}`).attr({
     'data-content': message,
     'data-trigger': 'manual',
-    'data-placement': 'auto',
+    'data-placement': position,
   });
-
   window.$(`#${elemId}`).popover('show');
-
-  setTimeout(closePopover, 2500);
 };
 
-export const closePopover = () => {
-  window.$(`.popover`).remove();
+const getCurrentElement = elemId =>
+  timerArray.length && timerArray.filter(el => el.elemId === elemId).length
+    ? timerArray.filter(el => el.elemId === elemId)[0]
+    : null;
+
+export const showPopover = (elemId, message, restParamets = {}) => {
+  const {
+    callback,
+    timeout = 2500,
+    timer = true,
+    position = 'auto',
+  } = restParamets;
+
+  const CurrentElement = getCurrentElement(elemId);
+
+  if (timer && !CurrentElement) {
+    setPopover(elemId, message, position);
+    const timerId = setTimeout(() => closePopover(elemId, callback), timeout);
+    setNewCurrentElement(elemId, timerId);
+  }
+
+  if (!timer) {
+    setPopover(elemId, message, position);
+    setNewCurrentElement(elemId);
+  }
+};
+
+export const closePopover = (elemId, callback) => {
+  // processing a call from the <Popover /> component in app/ErrorBoundary
+  // delete all popovers
+  if (typeof elemId === 'object' && Object.keys(elemId).length === 0) {
+    timerArray.forEach(el => el.timerId && clearTimeout(el.timerId));
+    timerArray = [];
+    window.$(`.popover`).remove();
+  } else {
+    // delete one popover with elemId
+    const currentTimerId = getCurrentElement(elemId)?.timerId || null;
+    window.$(`#${elemId}`).popover('dispose');
+    clearTimeout(currentTimerId);
+    timerArray = timerArray.filter(el => el.elemId !== elemId);
+  }
+
+  if (typeof callback === 'function') callback();
+
   return null;
 };
