74b96a0e48e108c4ddb761d63745e7a31551b006
Sergey Kashko <kashko.s.a@yandex.by>
2020-09-30 15:25:30 +0300

    [Peer-731] prevent second answer on the question (#281)

--- app/components/AnswerForm/index.js
+++ app/components/AnswerForm/index.js
@@ -18,16 +18,21 @@ import { strLength25x30000, required } from 'components/FormFields/validate';
 import { makeSelectLocale } from 'containers/LanguageProvider/selectors';
 import { makeSelectProfileInfo } from 'containers/AccountProvider/selectors';
 
-import { TEXT_SECONDARY } from 'style-constants';
-import { ANSWER_TYPE_FORM, TEXT_EDITOR_ANSWER_FORM } from './constants';
-
-import Wrapper from '../FormFields/Wrapper';
-import Span from '../Span';
-import Checkbox from '../Input/Checkbox';
+import Wrapper from 'components/FormFields/Wrapper';
+import Span from 'components/Span';
+import Checkbox from 'components/Input/Checkbox';
 import TextBlock from 'components/FormFields/TextBlock';
 import TextEditorField from 'components/FormFields/TextEditorField';
 import Button from 'components/Button/Contained/InfoLarge';
 import FormBox from 'components/Form';
+
+import {
+  TEXT_SECONDARY,
+  TEXT_PRIMARY,
+  BG_PRIMARY_TRANSPARENT,
+  BG_PRIMARY_LIGHT,
+} from 'style-constants';
+import { ANSWER_TYPE_FORM, TEXT_EDITOR_ANSWER_FORM } from './constants';
 
 export const PreviewWrapper = styled.div`
   background: linear-gradient(to right, #dcdcdc 50%, rgba(255, 255, 255, 0) 0%),
@@ -38,6 +43,43 @@ export const PreviewWrapper = styled.div`
   background-repeat: repeat-x, repeat-y;
   background-size: 8px 1px, 1px 8px;
   padding: 12px 0;
+`;
+
+const ModalDiv = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+  background-color: ${BG_PRIMARY_LIGHT};
+  background-image: linear-gradient(
+    45deg,
+    ${BG_PRIMARY_TRANSPARENT} 15%,
+    transparent 15%,
+    transparent 35%,
+    ${BG_PRIMARY_TRANSPARENT} 35%,
+    ${BG_PRIMARY_TRANSPARENT} 65%,
+    transparent 65%,
+    transparent 85%,
+    ${BG_PRIMARY_TRANSPARENT} 85%
+  );
+  background-size: 50px 50px;
+  background-repeat: repeat;
+  opacity: 0.4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalMessage = styled.p`
+  color: ${TEXT_PRIMARY};
+  font-size: 1.5em;
+  font-weight: bold;
+  position: relative;
+  top: -50px;
+  margin: 0 40px;
+  text-align: center;
 `;
 
 export const AnswerForm = ({
@@ -51,8 +93,16 @@ export const AnswerForm = ({
   textEditorValue,
   answerTypeLabel,
   isOfficialRepresentative,
+  isAnswered,
 }) => (
   <FormBox onSubmit={handleSubmit(sendAnswer)}>
+    {isAnswered && (
+      <ModalDiv>
+        <ModalMessage>
+          <FormattedMessage {...messages.questionIsAnswered} />
+        </ModalMessage>
+      </ModalDiv>
+    )}
     <Field
       name={TEXT_EDITOR_ANSWER_FORM}
       component={TextEditorField}
@@ -83,7 +133,11 @@ export const AnswerForm = ({
         )}
       </PreviewWrapper>
     </Wrapper>
-    <Button id={sendButtonId} disabled={sendAnswerLoading} type="submit">
+    <Button
+      id={sendButtonId}
+      disabled={sendAnswerLoading || isAnswered}
+      type="submit"
+    >
       {submitButtonName}
     </Button>
   </FormBox>
@@ -103,6 +157,7 @@ AnswerForm.propTypes = {
   isOfficialRepresentative: PropTypes.bool,
   properties: PropTypes.array,
   questionView: PropTypes.bool,
+  isAnswered: PropTypes.bool,
 };
 
 const FormClone = reduxForm({
