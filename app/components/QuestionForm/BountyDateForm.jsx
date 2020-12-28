7b9da1e8a76c9353bc3c18ad1619a33b34a3e226
Gleb Klimov <klimovgleb@mail.ru>
2020-12-28 13:57:32 +0300

    PEER-872 Fix bounty

--- app/components/QuestionForm/BountyDateForm.jsx
+++ app/components/QuestionForm/BountyDateForm.jsx
@@ -5,18 +5,9 @@ import { intlShape } from 'react-intl';
 
 import styled from 'styled-components';
 
-import {
-  required,
-  valueHasToBeLessThan,
-  number1x7,
-  number1x24,
-} from 'components/FormFields/validate';
+import { required, number1x168 } from 'components/FormFields/validate';
 
-import {
-  FORM_BOUNTY_DAYS,
-  FORM_BOUNTY_HOURS,
-  FORM_COMMUNITY,
-} from './constants';
+import { FORM_BOUNTY_HOURS, FORM_COMMUNITY } from './constants';
 import messages from './messages';
 import NumberInputField from '../FormFields/NumberInputField';
 
@@ -24,13 +15,7 @@ const BountyContainer = styled.div`
   margin-top: 20px;
 `;
 
-const BountyDateForm = ({
-  questionLoading,
-  intl,
-  formValues,
-  dotRestriction,
-  show,
-}) => {
+const BountyDateForm = ({ questionLoading, intl, formValues, show }) => {
   const bountyDisabled = useMemo(
     () => questionLoading || !formValues?.[FORM_COMMUNITY]?.value,
     [formValues, questionLoading],
@@ -38,27 +23,14 @@ const BountyDateForm = ({
   return show ? (
     <BountyContainer>
       <Field
-        name={FORM_BOUNTY_DAYS}
-        label={intl.formatMessage(messages.bountyDaysLabel)}
-        tip={intl.formatMessage(messages.bountyDaysTip)}
-        placeholder={intl.formatMessage(messages.daysPlaceholder)}
-        dotRestriction={dotRestriction}
-        component={NumberInputField}
-        disabled={bountyDisabled}
-        validate={[required, valueHasToBeLessThan, number1x7]}
-        warn={[required, valueHasToBeLessThan, number1x7]}
-        splitInHalf
-      />
-      <Field
         name={FORM_BOUNTY_HOURS}
         label={intl.formatMessage(messages.bountyHoursLabel)}
         tip={intl.formatMessage(messages.bountyHoursTip)}
         placeholder={intl.formatMessage(messages.hoursPlaceholder)}
-        dotRestriction={dotRestriction}
         component={NumberInputField}
         disabled={bountyDisabled}
-        validate={[required, valueHasToBeLessThan, number1x24]}
-        warn={[required, valueHasToBeLessThan, number1x24]}
+        validate={[required, number1x168]}
+        warn={[required, number1x168]}
         splitInHalf
       />
     </BountyContainer>
@@ -70,7 +42,6 @@ BountyDateForm.propTypes = {
   show: PropTypes.bool,
   intl: intlShape.isRequired,
   formValues: PropTypes.object,
-  dotRestriction: PropTypes.number,
 };
 
 export default memo(BountyDateForm);
