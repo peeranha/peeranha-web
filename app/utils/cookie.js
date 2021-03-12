a473c1308e1e61dcfd0a307ea9ae3efa28362e09
Sergey Kashko <kashko.s.a@yandex.by>
2021-03-12 09:06:31 +0300

    [peer-1000] Set referral link to cookie on testcommunity.net

--- app/utils/cookie.js
+++ app/utils/cookie.js
@@ -1,6 +1,7 @@
 export const NEVER_EXPIRES = 'Tue, 19 Jan 2038 01:14:07 GMT';
 export const DEFAULT_PATH = '/';
 export const TELOS_DOMAIN = '.telos.net';
+export const TEST_COMM_DOMAIN = 'testcommunity.net';
 
 export const getCookie = name => {
   const matches = document.cookie.match(
@@ -30,11 +31,16 @@ export const setCookie = ({
     if (optionKey === 'neverExpires') {
       return `${acc}; expires=${NEVER_EXPIRES}`;
     } else if (optionKey === 'allowSubdomains' && process.env.ENV !== 'dev') {
-      return `${acc}; domain=${
-        window.location.origin.endsWith(TELOS_DOMAIN)
-          ? TELOS_DOMAIN
-          : process.env.COOKIE_DOMAIN
-      }`;
+      let domain = process.env.COOKIE_DOMAIN;
+
+      if (window.location.origin.endsWith(TELOS_DOMAIN)) {
+        domain = TELOS_DOMAIN;
+      }
+      if (window.location.origin.endsWith(TEST_COMM_DOMAIN)) {
+        domain = TEST_COMM_DOMAIN;
+      }
+
+      return `${acc}; domain=${domain}`;
     } else if (optionKey === 'defaultPath') {
       return `${acc}; path=${DEFAULT_PATH}`;
     }
