067a698eccc7ddbe50ca86f2a24b64981c4a3622
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2021-04-23 12:32:19 +0300

    Fix translation (#579)

--- app/containers/Wallet/BoostBanner.jsx
+++ app/containers/Wallet/BoostBanner.jsx
@@ -23,16 +23,45 @@ import Wrapper from 'components/Banner';
 import Button from 'components/Button/Contained/InfoLarge';
 import A from 'components/A';
 import Span from 'components/Span';
+import { translationMessages } from '../../i18n';
 
 const GoToBoostPage = Button.extend`
   margin-top: 10px;
 
   :hover {
-    color: ${TEXT_LIGHT}
+    color: ${TEXT_LIGHT};
   }
 `.withComponent(Link);
 
-const BoostBanner = ({ userId }) => (    
+const whatABoostIs = locale => (
+  <A to={routes.faq(getQuestionCode(SECTION_ID, ...WHAT_IS_BOOST.split('.')))}>
+    <Span color={TEXT_PRIMARY}>
+      {translationMessages[locale][messages.whatABoostIs.id]}
+    </Span>
+  </A>
+);
+
+const howToStake = locale => (
+  <A
+    to={routes.faq(
+      getQuestionCode(SECTION_ID, ...HOW_TO_MAKE_A_STAKE.split('.')),
+    )}
+  >
+    <Span color={TEXT_PRIMARY}>
+      {translationMessages[locale][messages.howToStake.id]}
+    </Span>
+  </A>
+);
+
+const FAQs = locale => (
+  <A to={routes.faq(getSectionCode(SECTION_ID, BOOST_SECTION))}>
+    <Span color={TEXT_PRIMARY}>
+      {translationMessages[locale][messages.FAQs.id]}
+    </Span>
+  </A>
+);
+
+const BoostBanner = ({ userId, locale }) => (
   <Wrapper className="mt-3">
     <img src={activateBoostBanner} alt="boost-banner" />
 
@@ -42,13 +71,14 @@ const BoostBanner = ({ userId }) => (
       </p>
 
       <p>
-        Learn <A to={routes.faq(getQuestionCode(SECTION_ID, ...WHAT_IS_BOOST.split('.')))}>
-          <Span color={TEXT_PRIMARY}>what a Boost is</Span>
-        </A> and <A to={routes.faq(getQuestionCode(SECTION_ID, ...HOW_TO_MAKE_A_STAKE.split('.')))}>
-          <Span color={TEXT_PRIMARY}>how you place a stake</Span>
-        </A> in the <A to={routes.faq(getSectionCode(SECTION_ID, BOOST_SECTION))}>
-          <Span color={TEXT_PRIMARY}>FAQs</Span>
-        </A>.
+        <FormattedMessage
+          {...messages.boostHelp}
+          values={{
+            whatABoostIs: whatABoostIs(locale),
+            howToStake: howToStake(locale),
+            FAQs: FAQs(locale),
+          }}
+        />
       </p>
 
       <GoToBoostPage to={routes.userBoost(userId)}>
