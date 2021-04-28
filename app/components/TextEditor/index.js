7ed2516f2e265c47ca406fe930e1329a5b196eee
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2021-04-28 11:58:54 +0300

    Fix highlighted russian words (#583)

--- app/components/TextEditor/index.js
+++ app/components/TextEditor/index.js
@@ -9,9 +9,12 @@ import PropTypes from 'prop-types';
 import SimpleMDE from 'react-simplemde-editor';
 import EditorOptions from 'simplemde';
 import 'easymde/dist/easymde.min.css';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import options from './options';
-
+import { makeSelectLocale } from '../../containers/LanguageProvider/selectors';
+import { DEFAULT_LOCALE } from '../../i18n';
 const TEXT_EDITOR_CLASSNAME = 'component-text-editor';
 
 /* eslint no-return-assign: "error" */
@@ -23,12 +26,13 @@ class TextEditor extends React.PureComponent {
   static getHtmlText = md => EditorOptions.prototype.markdown(md);
 
   render() {
+    const { locale } = this.props;
     return (
       <SimpleMDE
         {...this.props}
         className={TEXT_EDITOR_CLASSNAME}
         onBlur={this.onBlurHandler}
-        options={options}
+        options={{ ...options, spellChecker: locale === DEFAULT_LOCALE }}
         extraKeys={{
           Tab: false,
         }}
@@ -44,7 +48,12 @@ TextEditor.propTypes = {
   onChange: PropTypes.func,
   onBlur: PropTypes.func,
   value: PropTypes.string,
+  locale: PropTypes.string,
 };
 
 export { TEXT_EDITOR_CLASSNAME };
-export default TextEditor;
+export default connect(
+  createStructuredSelector({
+    locale: makeSelectLocale(),
+  }),
+)(TextEditor);
