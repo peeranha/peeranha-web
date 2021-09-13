0dc59088a865d13d8a35d826754d38ee013a0f5e
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2021-09-13 13:29:08 +0300

    Add logout (#616)

--- app/containers/Logout/saga.js
+++ app/containers/Logout/saga.js
@@ -2,10 +2,9 @@ import { takeLatest, call, put, select } from 'redux-saga/effects';
 
 import createdHistory from 'createdHistory';
 import * as routes from 'routes-config';
-import { deleteCookie, getCookie } from 'utils/cookie';
+import { deleteCookie } from 'utils/cookie';
 
 import { AUTOLOGIN_DATA, PROFILE_INFO_LS } from 'containers/Login/constants';
-import { selectEos } from 'containers/EosioProvider/selectors';
 import {
   getCurrentAccountSuccess,
   addLoginData,
@@ -15,21 +14,16 @@ import { LOGOUT } from './constants';
 
 import { logoutSuccess, logoutErr } from './actions';
 import { clearNotificationsData } from '../../components/Notifications/actions';
+import { selectEthereum } from '../EthereumProvider/selectors';
 
 export function* logoutWorker() {
   try {
-    const eosService = yield select(selectEos);
-    const autoLoginData = JSON.parse(getCookie(AUTOLOGIN_DATA) || null);
+    const ethereumService = yield select(selectEthereum);
 
     deleteCookie(AUTOLOGIN_DATA);
     deleteCookie(PROFILE_INFO_LS);
 
-    if (autoLoginData?.loginWithFacebook && window.FB) {
-      window.FB.logout(() => {});
-    }
-    yield call(eosService.resetKeycatUserData);
-    yield call(eosService.forgetIdentity);
-    yield call(eosService.initEosioWithoutScatter);
+    yield call(ethereumService.resetMetaMaskUserData);
 
     yield call(createdHistory.push, routes.questions());
     yield put(getCurrentAccountSuccess());
