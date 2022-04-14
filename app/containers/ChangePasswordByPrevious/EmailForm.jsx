bf7c03697fd3e66b4fb3437c53354a5b9be2ffdb
DY-JS <delo1604@ukr.net>
2022-04-14 13:02:12 +0300

    PEER20-266-Edit Question headers and style of selected button in new Post page

---
+++ app/containers/ChangePasswordByPrevious/EmailForm.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Field, reduxForm } from 'redux-form/immutable';
+import { FormattedMessage } from 'react-intl';
+import { translationMessages } from 'i18n';
+
+import commonMessages from 'common-messages';
+import { scrollToErrorField } from 'utils/animation';
+
+import H4 from 'components/H4';
+import TextInputField from 'components/FormFields/TextInputField';
+import Button from 'components/Button/Contained/InfoLarge';
+import signUpMessages from 'containers/SignUp/messages';
+
+import { validateEmail, required } from 'components/FormFields/validate';
+
+import { EMAIL_FIELD, EMAIL_FORM } from './constants';
+
+const EmailForm = ({
+  handleSubmit,
+  locale,
+  sendEmail,
+  sendEmailProcessing,
+}) => (
+  <div>
+    <H4 className="text-center pb-3">
+      <FormattedMessage {...commonMessages.change} />{' '}
+      <FormattedMessage {...signUpMessages.password} />
+    </H4>
+
+    <form onSubmit={handleSubmit(sendEmail)}>
+      <Field
+        name={EMAIL_FIELD}
+        disabled
+        label={translationMessages[locale][signUpMessages.email.id]}
+        component={TextInputField}
+        validate={[validateEmail, required]}
+        warn={[validateEmail, required]}
+      />
+
+      <Button disabled={sendEmailProcessing} className="w-100 mb-3">
+        <FormattedMessage {...commonMessages.submit} />
+      </Button>
+    </form>
+  </div>
+);
+
+EmailForm.propTypes = {
+  handleSubmit: PropTypes.func,
+  sendEmail: PropTypes.func,
+  locale: PropTypes.string,
+  sendEmailProcessing: PropTypes.bool,
+};
+
+/* eslint import/no-mutable-exports: 0 */
+let FormClone = reduxForm({
+  form: EMAIL_FORM,
+  onSubmitFail: errors => scrollToErrorField(errors),
+})(EmailForm);
+
+FormClone = connect((_, props) => ({
+  enableReinitialize: true,
+  initialValues: {
+    [EMAIL_FIELD]: props.loginData.email,
+  },
+}))(FormClone);
+
+export default FormClone;
---
+++ app/containers/Questions/Content/Body/ExpertPopover.jsx
@@ -0,0 +1,54 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import commonMessages from 'common-messages';
+import DescriptionList from 'components/DescriptionList';
+import { BG_LIGHT, BORDER_PRIMARY_LIGHT, TEXT_DARK } from 'style-constants';
+
+const Base = styled.div`
+  position: absolute;
+  background-color: ${BG_LIGHT};
+  width: 290px;
+  z-index: 100;
+  left: -235px;
+  top: 25px;
+  border-radius: 5px;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.3);
+  padding: 15px;
+
+  span {
+    color: ${TEXT_DARK};
+  }
+
+  > ul {
+    li {
+      color: ${TEXT_DARK};
+      margin-left: 15px;
+    }
+
+    li::before {
+      content: '\\2022';
+      color: ${BORDER_PRIMARY_LIGHT};
+      font-weight: bold;
+      display: inline-block;
+      width: 1em;
+      margin-left: -1em;
+    }
+  }
+`;
+
+const ExpertPopover = ({ locale }) => (
+  <Base>
+    <DescriptionList
+      locale={locale}
+      label={commonMessages.expertPopoverLabel.id}
+      items={commonMessages.expertPopoverList.id}
+    />
+  </Base>
+);
+
+ExpertPopover.propTypes = {
+  locale: PropTypes.string,
+};
+
+export default memo(ExpertPopover);
