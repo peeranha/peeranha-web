9d94bc19b10a9e790c5914b52f23d9bc2a05aa87
Andrei Larin <61755737+andreilarin@users.noreply.github.com>
2022-05-05 16:52:53 +0400

    PEER-371 (#771)
    Co-authored-by: gigant-misli <61755737+gigant-misli@users.noreply.github.com>

--- app/containers/LeftMenu/MobileAutorizationButtons.jsx
+++ app/containers/LeftMenu/MobileAutorizationButtons.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import createdHistory from 'createdHistory';
-import * as routes from 'routes-config';
 
 import messages from 'common-messages';
 
 import LargeOutlinedButton from 'components/Button/Outlined/InfoLarge';
 
-export default React.memo(({ profile, isMenuVisible, showLoginModal }) => {
+export default ({ profile, isMenuVisible, showLoginModal }) => {
   if (profile || !isMenuVisible) {
     return null;
   }
@@ -15,15 +13,8 @@ export default React.memo(({ profile, isMenuVisible, showLoginModal }) => {
   return (
     <div className="d-flex align-items-center">
       <LargeOutlinedButton onClick={showLoginModal}>
-        <FormattedMessage {...messages.login} />
-      </LargeOutlinedButton>
-
-      <LargeOutlinedButton
-        className="ml-2"
-        onClick={() => createdHistory.push(routes.signup.email.name)}
-      >
-        <FormattedMessage {...messages.signUp} />
+        <FormattedMessage id={messages.login.id} />
       </LargeOutlinedButton>
     </div>
   );
-});
+};
