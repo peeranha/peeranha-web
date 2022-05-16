e84b3055ff522bdb67ab38b8df9b62dc307219d7
Alexei Shulga <alexeishulg@gmail.com>
2022-05-16 13:26:30 +0300

    fix switching buttons

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -19,38 +19,39 @@ const Button = styled.button`
   pointer-events: ${x => (x.disabled ? 'none' : 'auto')};
   opacity: ${x => (x.disabled ? '0.5' : '1')};
 
-  :after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 5px;
-    height: 5px;
-    background: rgba(255, 255, 255, 0.5);
-    opacity: 0;
-    border-radius: 100%;
-    transform: scale(1, 1) translate(-50%);
-    transform-origin: 50% 50%;
-  }
+  // ToDo: switching buttons ALL/POSTS/ANSWERS;
+  // :after {
+  //   content: '';
+  //   position: absolute;
+  //   top: 50%;
+  //   left: 50%;
+  //   width: 5px;
+  //   height: 5px;
+  //   background: rgba(255, 255, 255, 0.5);
+  //   opacity: 0;
+  //   border-radius: 100%;
+  //   transform: scale(1, 1) translate(-50%);
+  //   transform-origin: 50% 50%;
+  // }
 
-  @keyframes ripple {
-    0% {
-      transform: scale(0, 0);
-      opacity: 1;
-    }
-    20% {
-      transform: scale(25, 25);
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-      transform: scale(40, 40);
-    }
-  }
+  // @keyframes ripple {
+  //   0% {
+  //     transform: scale(0, 0);
+  //     opacity: 1;
+  //   }
+  //   20% {
+  //     transform: scale(25, 25);
+  //     opacity: 1;
+  //   }
+  //   100% {
+  //     opacity: 0;
+  //     transform: scale(40, 40);
+  //   }
+  // }
 
-  :focus:not(:active)::after {
-    animation: ripple 1s ease-out;
-  }
+  // :focus:not(:active)::after {
+  //   animation: ripple 1s ease-out;
+  // }
 `;
 
 export default Button;
