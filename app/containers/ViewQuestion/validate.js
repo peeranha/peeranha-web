e9b65381a6e719ffb526826ee5bb40e91420838d
gigant-misli <61755737+gigant-misli@users.noreply.github.com>
2022-05-20 17:39:54 +0400

    hotfix/PEER-94

--- app/containers/ViewQuestion/validate.js
+++ app/containers/ViewQuestion/validate.js
@@ -132,7 +132,7 @@ export const postCommentValidator = (
   if (item.comments.length === maxCommentsNumber) {
     message = `${translations[messages.itemsMax.id]}`;
   } else if (
-    (item.user === profileInfo.user ||
+    (item.author.user === profileInfo.user ||
       questionData.author === profileInfo.user) &&
     getRatingByCommunity(profileInfo, communityId) < MIN_RATING_FOR_MY_ITEM
   ) {
