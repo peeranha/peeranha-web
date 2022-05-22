64db7383d37a0fcb5f8792f144a8f197db027139
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2022-05-22 21:32:46 +0300

    Add modal for meta transaction (#864)

--- app/containers/EthereumProvider/reducer.js
+++ app/containers/EthereumProvider/reducer.js
@@ -9,12 +9,15 @@ import {
   INIT_ETHEREUM,
   INIT_ETHEREUM_SUCCESS,
   INIT_ETHEREUM_ERROR,
+  SHOW_MODAL,
+  HIDE_MODAL,
 } from './constants';
 
 export const initialState = fromJS({
   initializing: false,
   ethereum: null,
   error: null,
+  showModal: false,
 });
 
 function ethereumProviderReducer(state = initialState, action) {
@@ -27,6 +30,10 @@ function ethereumProviderReducer(state = initialState, action) {
       return state.set('initializing', false).set('ethereum', ethereum);
     case INIT_ETHEREUM_ERROR:
       return state.set('initializing', false).set('error', error);
+    case SHOW_MODAL:
+      return state.set('showModal', true);
+    case HIDE_MODAL:
+      return state.set('showModal', false);
     default:
       return state;
   }
