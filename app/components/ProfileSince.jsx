820b2ffa4f0b8f679294f765c038601ff9b94443
DY-JS <delo1604@ukr.net>
2022-05-26 23:09:52 +0300

    PEER-95 Fix view of join date on  profile page

--- app/components/ProfileSince.jsx
+++ app/components/ProfileSince.jsx
@@ -1,10 +1,16 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import { getFormattedDate } from 'utils/datetime';
 import styled from 'styled-components';
 
 import { MONTH_3LETTERS__DAY_YYYY } from 'utils/constants';
+import messages from 'containers/Profile/messages';
+
+const Div = styled.div`
+  display: ${({ view }) => view === 'Invalid Date' && 'none'};
+`;
 
 const Span = styled.span`
   @media only screen and (min-width: 1354px) {
@@ -18,7 +24,14 @@ export const ProfileSince = ({ creationTime, locale }) => {
     [],
   );
 
-  return <Span>{profileSince}</Span>;
+  return (
+    <Div view={profileSince}>
+      <FormattedMessage {...messages.memberSince} />
+      <div>
+        <Span>{profileSince}</Span>
+      </div>
+    </Div>
+  );
 };
 
 ProfileSince.propTypes = {
