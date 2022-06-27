b76b45eaf5109e689f497da4c70a60d589069f52
Denys Romanov <lampard.ks@gmail.com>
2022-06-27 16:32:53 +0300

    Feature/peer 14 (#929)
    * PEER20-359 : update webpack for dev enviroment
    
    * PEER-14 : update webpack config
    
    * PEER-14 : update config babel
    
    * PEER-14 : update modules
    
    * PEER-14 : update loader and remove unused libs
    
    * PEER-14 : add storybook
    
    * PEER-14 : add example for storybook
    
    * PEER-14 : add new components and themes
    
    * PEER-14 : update deploy
    
    * PEER-14 : fix eslint
    
    * PEER-14 : update storybook
    
    * PEER-14 : fix package-lock.json
    
    * PEER-14 : fix page privacy policy
    
    * PEER-14 : fix errors
    
    Co-authored-by: Denys Romanov <denys.romanov@DENYSROMANOVL.local>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,26 @@
+/* eslint-disable-next-line */
 const fs = require('fs');
 
 const prettierOptions = JSON.parse(fs.readFileSync('./.prettierrc', 'utf8'));
-
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier'],
-  plugins: ['prettier', 'redux-saga', 'react', 'jsx-a11y'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/warnings',
+    'plugin:import/errors',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: [
+    'react',
+    'react-hooks',
+    'redux-saga',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'prettier',
+  ],
   env: {
     browser: true,
     node: true,
@@ -13,10 +28,24 @@ module.exports = {
     es6: true,
   },
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 7,
     sourceType: 'module',
     ecmaFeatures: {
+      modules: true,
       jsx: true,
+      forOf: true,
+      blockBindings: true,
+      arrowFunctions: true,
+      classes: true,
+      defaultParams: true,
+      destructuring: true,
+      generators: true,
+      restParams: true,
+      spread: true,
+      superInFunctions: true,
+      templateStrings: true,
+      experimentalObjectRestSpread: true,
+      experimentalDecorators: true,
     },
   },
   rules: {
@@ -28,21 +57,15 @@ module.exports = {
     'import/imports-first': 0,
     'import/newline-after-import': 0,
     'import/no-dynamic-require': 0,
-    'import/no-extraneous-dependencies': 0,
-    'import/no-named-as-default': 0,
-    'import/no-unresolved': 2,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/no-named-as-default': 'off',
+    'import/no-unresolved': 'off',
     'import/no-webpack-loader-syntax': 0,
-    'import/prefer-default-export': 0,
+    'import/prefer-default-export': 'off',
     'import/extensions': 0,
     'jsx-a11y/anchor-is-valid': 0,
     'react/jsx-no-bind': 0,
-    indent: [
-      2,
-      2,
-      {
-        SwitchCase: 1,
-      },
-    ],
+    indent: 'off',
     'jsx-a11y/aria-props': 2,
     'jsx-a11y/heading-has-content': 0,
     'jsx-a11y/label-has-for': 2,
@@ -56,7 +79,19 @@ module.exports = {
     'no-use-before-define': 0,
     'prefer-template': 2,
     'react/jsx-closing-tag-location': 0,
-    'react/forbid-prop-types': 0,
+    'react/prop-types': [
+      'off',
+      {
+        ignore: [
+          'className',
+          'children',
+          'dispatch',
+          'location',
+          'match',
+          'history',
+        ],
+      },
+    ],
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-filename-extension': 0,
     'react/jsx-no-target-blank': 0,
@@ -67,12 +102,83 @@ module.exports = {
     'redux-saga/no-yield-in-race': 2,
     'redux-saga/yield-effects': 2,
     'require-yield': 0,
+    'no-plusplus:': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/adjacent-overload-signatures': 'error',
+        '@typescript-eslint/ban-ts-comment': 'error',
+        '@typescript-eslint/ban-types': 'error',
+        camelcase: 'off',
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'function',
+            format: ['PascalCase', 'camelCase'],
+          },
+        ],
+        '@typescript-eslint/consistent-type-assertions': 'error',
+        '@typescript-eslint/member-delimiter-style': [
+          2,
+          {
+            multiline: {
+              delimiter: 'semi',
+            },
+            singleline: {
+              delimiter: 'semi',
+            },
+          },
+        ],
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'error',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'error',
+        '@typescript-eslint/no-empty-interface': 'error',
+        '@typescript-eslint/no-inferrable-types': 'error',
+        '@typescript-eslint/no-misused-new': 'error',
+        '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'warn',
+        '@typescript-eslint/no-this-alias': 'error',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': 'error',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/prefer-namespace-keyword': 'error',
+        '@typescript-eslint/triple-slash-reference': 'error',
+        '@typescript-eslint/type-annotation-spacing': 'error',
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'prefer-rest-params': 'error',
+        'prefer-spread': 'error',
+      },
+    },
+  ],
+
   settings: {
     'import/resolver': {
-      webpack: {
-        config: './internals/webpack/webpack.prod.babel.js',
+      'babel-module': {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['app'],
+      },
+      alias: [
+        ['components', './app/components'],
+        ['common-components', './app/components/common'],
+        ['styles', './app/styles'],
+        ['hooks', './app/hooks'],
+        ['icons', './app/components/icons'],
+        ['themes', './app/themes'],
+      ],
     },
   },
 };
