1c7ad25262366d3e2f22ae33164db98d01a9b5f3
gigant-misli <61755737+gigant-misli@users.noreply.github.com>
2022-06-28 12:03:52 +0400

    fix errors

--- app/containers/Users/saga.js
+++ app/containers/Users/saga.js
@@ -10,15 +10,17 @@ export function* getUsersWorker({ loadMore, reload, communityId }) {
     const limit = yield select(selectLimit());
     const sorting = yield select(selectSorting());
     const skip = reload ? 0 : yield select(selectSkip());
-    let users = yield getUsers({ limit, skip, sorting });
 
-    if (!Number.isNaN(communityId))
-      users = users.filter(user =>
-        user.followedCommunities
-          .map(item => Number(item))
-          .includes(communityId),
-      );
-    yield put(getUsersSuccess(users, loadMore, reload));
+    const users = yield getUsers({ limit, skip, sorting });
+    const filteredUsers = Number.isNaN(communityId)
+      ? users
+      : users.filter(user =>
+          user.followedCommunities
+            .map(item => Number(item))
+            .includes(communityId),
+        );
+
+    yield put(getUsersSuccess(filteredUsers, loadMore, reload));
   } catch (err) {
     yield put(getUsersErr(err));
   }
