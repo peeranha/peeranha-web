bd9d4a9bccac5cf2f190d2969f32f302fda1a9df
aweawe95 <vitalikarach95@gmail.com>
2022-07-15 14:48:42 +0300

    remove Z

--- app/components/Icon/IconWithSizes.js
+++ app/components/Icon/IconWithSizes.js
@@ -8,5 +8,6 @@ const IconMd = props => <Icon width="18" {...props} />;
 const IconEm = props => <Icon width="19" {...props} />;
 const IconLm = props => <Icon width="20" {...props} />;
 const IconLg = props => <Icon width="24" {...props} />;
+const IconXl = props => <Icon width="140" {...props} />;
 
-export { IconSm, IconXm, IconMd, IconEm, IconLm, IconLg };
+export { IconSm, IconXm, IconMd, IconEm, IconLm, IconLg, IconXl };
