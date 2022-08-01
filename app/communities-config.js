dc1b09e4e4f8ff3c7f314161a39b6312625e6bd6
YuriyLu <yura2000uraura@gmail.com>
2022-08-01 21:41:43 +0300

    Revert communities-config changing

--- app/communities-config.js
+++ app/communities-config.js
@@ -82,10 +82,8 @@ const communitiesConfig = {
     // },
   },
   dev: {
-    1: {
-      origin: 'http://localhost:3000',
-      // src: PolygonLogo,
-      // styles: PolygonStyles,
+    3: {
+      origin: 'http://localhost:3100',
     },
   },
 };
