b3fc3f8b50ab8878b25f0d5cfe65067c13ac4152
Vitali Karach <81355129+AweAwe95@users.noreply.github.com>
2022-08-16 19:19:33 +0300

    add functionland community (#1061)

---
+++ app/communities-configs/functionland/index.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import { css } from 'styled-components';
+
+import FunctionlandLogo from './images/functionland-logo.svg?inline';
+import favicon from '!file-loader?name=[name].[ext]!./images/favicon-functionland.ico';
+
+const domainName = 'https://fx.land/';
+
+const fontSet = 'Roboto-Regular, Arial, sans-serif';
+
+export const customSubHeaderConfig = {
+  design: 'functionland_style',
+  styles: {
+    bg: {
+      header: '#FFFFFF',
+      dropdown: '#FFFFFF',
+    },
+    color: {
+      a: '#999999',
+    },
+    font: {
+      body: fontSet,
+    },
+    header: {
+      background: '#FFFFFF',
+    },
+    subHeader: css`
+      border-bottom: 1px solid #cecece;
+      a,
+      span {
+        transition: all 0.3s ease-out;
+      }
+      a:hover,
+      span:hover {
+        color: #000000 !important;
+        opacity: 1 !important;
+      }
+    `,
+    subitems: css`
+      left: 50% !important;
+      top: 100% !important;
+      padding: 25px 30px !important;
+      border-radius: 3px !important;
+      min-width: 100% !important;
+      transform: translate(-50%, -5%) !important;
+
+      animation: ani 0.3s both;
+      @keyframes ani {
+        0% {
+          opacity: 0;
+          transform: translate(-50%, -10%);
+        }
+        100% {
+          opacity: 1;
+          transform: translate(-50%, -5%);
+        }
+      }
+
+      ::before {
+        content: '';
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        top: -10px;
+        left: 50%;
+        transform: translateX(-50%) rotate(45deg);
+        transition: background-color 0.3s;
+        border-top: 1px solid #f2f2f2;
+        border-left: 1px solid #f2f2f2;
+        background: #ffffff;
+        z-index: 9999;
+      }
+
+      @media only screen and (max-width: 991px) {
+        a {
+          padding: 0 0 0 30px !important;
+        }
+      }
+
+      @media only screen and (min-width: 992px) {
+        a {
+          padding: 5px 0 !important;
+          line-height: 1.6 !important;
+        }
+      }
+    `,
+    subHeaderItem: css`
+      position: relative;
+
+      @media only screen and (min-width: 900px) {
+        font-size: 14px;
+      }
+      @media only screen and (min-width: 1440px) {
+        font-size: 15px;
+      }
+
+      font-weight: bold;
+
+      > div {
+        font-weight: normal;
+      }
+
+      padding: 10px 15px !important;
+
+      @media only screen and (min-width: 992px) {
+        padding: 0;
+        margin-left: 30px;
+
+        :first-child {
+          margin-left: 0;
+        }
+      }
+
+      margin: 0 !important;
+    `,
+    CustomSubHeader: css`
+      margin: 0 !important;
+    `,
+    subHeaderContainerStyles: css``,
+    topLogoContainerStyles: css`
+      width: 80px !important;
+      margin: 0 !important;
+      margin-right: 60px !important;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+    `,
+    subHeaderLogoStyles: css`
+      height: 40px;
+
+      > img {
+        height: 100%;
+      }
+    `,
+
+    Highlighted: css``,
+  },
+  links: [
+    {
+      text: 'Store',
+      href: `${domainName}/store/`,
+    },
+    {
+      text: 'Provide',
+      href: `${domainName}/provide/`,
+    },
+    {
+      text: 'Build',
+      href: `${domainName}/build/`,
+      subitems: [
+        {
+          text: 'Docs',
+          href: `${domainName}/build/#benefits-banner`,
+        },
+        {
+          text: 'Tools & services',
+          href: `${domainName}/build/#tools-and-more`,
+        },
+        {
+          text: 'Grants',
+          href: `${domainName}/build/#grants`,
+        },
+        {
+          text: 'Roadmap',
+          href: `${domainName}/build/#usp`,
+        },
+        {
+          text: 'Videos',
+          href: `${domainName}/build/#videos`,
+        },
+        {
+          text: 'Filecoin Community',
+          href: `${domainName}/build/#community`,
+        },
+        {
+          text: 'Events',
+          href: `${domainName}/build/#events`,
+        },
+      ],
+    },
+    {
+      text: 'Blog',
+      href: `${domainName}/provide/`,
+    },
+  ],
+};
+
+export const FunctionlandStyles = {
+  withoutCopyright: true,
+  withoutSubHeader: true,
+  withoutAdditionalLinks: true,
+  poweredByPeeranha: true,
+  signUpPageLogo: FunctionlandLogo,
+  favicon,
+  colors: {
+    mainBackground: '#F3F3F3',
+    linkColor: 'rgb(9, 147, 147)',
+    linkColorTransparent: 'rgb(48, 103, 103)',
+    headerPrimary: 'rgb(9, 147, 147)',
+    commentOption: 'rgb(9, 147, 147)',
+    contentHeader: 'rgb(9, 147, 147)',
+    blockedInfoArea: 'rgb(48, 103, 103)',
+    transparentIconColor: 'rgb(209, 255, 255)',
+    loaderColor: 'rgb(9, 147, 147)',
+    votingIconColor: 'rgb(9, 147, 147)',
+    linkColorSecondary: 'rgb(9, 147, 147)',
+    walletButton: 'rgb(9, 147, 147)',
+    btnColor: 'rgb(9, 147, 147)',
+    tagColor: 'rgb(9, 147, 147)',
+  },
+  headerHeight: 80,
+  domainName,
+};
