439f6c2dc6630281764a22518d4217645e2d6c3c
Alexei <68242938+alexeishulga@users.noreply.github.com>
2022-09-08 16:36:58 +0300

    fix video block (#1085)

--- app/containers/HomePage/VideoSection/VideoSection.tsx
+++ app/containers/HomePage/VideoSection/VideoSection.tsx
@@ -8,14 +8,16 @@ const VideoSection: React.FC = (): JSX.Element => {
     <div css={css(styles.background)}>
       <div id={VIDEO_SCREEN} css={css(styles.videoSection)}>
         <div className="df jcc" css={css(styles.videoBlock)}>
-          <iframe
-            css={css(styles.videoIframe)}
-            src="https://www.youtube.com/embed/Bvt42RqWFKc"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          />
+          <div css={css(styles.videoMobile)}>
+            <iframe
+              css={css(styles.videoIframe)}
+              src="https://www.youtube.com/embed/Bvt42RqWFKc"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            />
+          </div>
         </div>
       </div>
     </div>
