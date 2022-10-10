5033cfff39f589fead4f7902ca9c687de4b48efc
aweawe95 <vitalikarach95@gmail.com>
2022-10-10 15:20:05 +0300

    fix Users link for Protocol Admin account

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -87,7 +87,10 @@ import { getCookie, setCookie } from '../../utils/cookie';
 import { REFERRAL_CODE_URI } from './constants';
 import { AUTOLOGIN_DATA } from '../Login/constants';
 import { redirectToFeed } from './actions';
-import { hasGlobalModeratorRole } from '../../utils/properties';
+import {
+  hasGlobalModeratorRole,
+  hasProtocolAdminRole,
+} from '../../utils/properties';
 import CookieConsentPopup from '../../components/CookieConsentPopup';
 
 const single = isSingleCommunityWebsite();
@@ -371,7 +374,7 @@ const App = ({
             render={props => Wrapper(EditAnswer, props)}
           />
 
-          {(hasGlobalModeratorRole() || single) && (
+          {(hasGlobalModeratorRole() || hasProtocolAdminRole() || single) && (
             <Route
               exact
               path={routes.users()}
