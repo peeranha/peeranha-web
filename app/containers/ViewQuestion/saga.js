64afd990717c71b9d7b261633bb805862020c390
alexeishulga <alexeishulg@gmail.com>
2022-11-08 17:31:39 +0300

    fix post answer

--- app/containers/ViewQuestion/saga.js
+++ app/containers/ViewQuestion/saga.js
@@ -153,9 +153,9 @@ import { selectEthereum } from '../EthereumProvider/selectors';
 import { getQuestionFromGraph } from '../../utils/theGraph';
 
 import { selectPostedAnswerIds } from '../AskQuestion/selectors';
-export const isGeneralQuestion = (question) => Boolean(question.postType === 1);
+export const isGeneralQuestion = question => Boolean(question.postType === 1);
 
-const getPostsRoute = (postType) => {
+const getPostsRoute = postType => {
   switch (postType) {
     case 0:
       return routes.expertPosts();
@@ -166,15 +166,14 @@ const getPostsRoute = (postType) => {
   }
 };
 
-export const getQuestionTypeValue = (postType) =>
+export const getQuestionTypeValue = postType =>
   postType === POST_TYPE.generalPost
     ? POST_TYPE.expertPost
     : POST_TYPE.generalPost;
 
 const isOwnItem = (questionData, profileInfo, answerId) =>
   questionData.author.user === profileInfo.user ||
-  questionData.answers.find((x) => x.id === answerId)?.user ===
-    profileInfo.user;
+  questionData.answers.find(x => x.id === answerId)?.user === profileInfo.user;
 
 export function* getQuestionData({
   questionId,
@@ -191,7 +190,7 @@ export function* getQuestionData({
     question = yield call(getQuestionById, ethereumService, questionId, user);
     if (question.officialReply) {
       const officialReply = question.answers.find(
-        (answer) => answer.id === question.officialReply,
+        answer => answer.id === question.officialReply,
       );
       if (officialReply) {
         officialReply.isOfficialReply = true;
@@ -217,13 +216,13 @@ export function* getQuestionData({
     }
 
     yield all(
-      question.answers.map(function* (answer) {
+      question.answers.map(function*(answer) {
         answer.commentCount = answer.comments.length;
         answer.id = Number(answer.id.split('-')[1]);
 
         answer.author = { ...answer.author, user: answer.author.id };
 
-        answer.comments = answer.comments.map((comment) => ({
+        answer.comments = answer.comments.map(comment => ({
           ...comment,
           author: { ...comment.author, user: comment.author.id },
           id: Number(comment.id.split('-')[2]),
@@ -244,7 +243,7 @@ export function* getQuestionData({
       }),
     );
 
-    question.comments = question.comments.map((comment) => ({
+    question.comments = question.comments.map(comment => ({
       ...comment,
       author: { ...comment.author, user: comment.author.id },
       id: Number(comment.id.split('-')[2]),
@@ -317,11 +316,11 @@ export function* getQuestionData({
 
   function* processAnswers() {
     yield all(
-      question.answers.map(function* (x) {
+      question.answers.map(function*(x) {
         yield call(addOptions, x);
 
         yield all(
-          x.comments.map(function* (y) {
+          x.comments.map(function*(y) {
             yield call(addOptions, y);
           }),
         );
@@ -331,7 +330,7 @@ export function* getQuestionData({
 
   function* processCommentsOfQuestion() {
     yield all(
-      question.comments.map(function* (y) {
+      question.comments.map(function*(y) {
         yield call(addOptions, y);
       }),
     );
@@ -348,13 +347,13 @@ export function* getQuestionData({
   // To avoid of fetching same user profiles - remember it and to write author here
   if ((user && isQuestionChanged) || isQuestionJustCreated) {
     yield all(
-      Array.from(users.keys()).map(function* (userFromItem) {
+      Array.from(users.keys()).map(function*(userFromItem) {
         const author = yield call(getUserProfileWorker, {
           user: userFromItem,
           getFullProfile: true,
           communityIdForRating: question.communityId,
         });
-        users.get(userFromItem).map((cachedItem) => {
+        users.get(userFromItem).map(cachedItem => {
           cachedItem.author = author;
         });
       }),
@@ -394,8 +393,13 @@ export function* saveCommentWorker({
   buttonId,
 }) {
   try {
-    const { questionData, ethereumService, profileInfo, locale, histories } =
-      yield call(getParams);
+    const {
+      questionData,
+      ethereumService,
+      profileInfo,
+      locale,
+      histories,
+    } = yield call(getParams);
 
     yield call(isAvailableAction, () =>
       editCommentValidator(profileInfo, buttonId, translationMessages[locale]),
@@ -419,11 +423,11 @@ export function* saveCommentWorker({
     let item;
 
     if (answerId === 0) {
-      item = questionData.comments?.find((x) => x.id === commentId);
+      item = questionData.comments?.find(x => x.id === commentId);
     } else if (answerId > 0) {
       item = questionData.answers
-        .find((x) => x.id === answerId)
-        .comments.find((x) => x.id === commentId);
+        .find(x => x.id === answerId)
+        .comments.find(x => x.id === commentId);
     }
 
     const newHistory = {
@@ -457,8 +461,13 @@ export function* deleteCommentWorker({
   buttonId,
 }) {
   try {
-    const { questionData, ethereumService, locale, profileInfo, histories } =
-      yield call(getParams);
+    const {
+      questionData,
+      ethereumService,
+      locale,
+      profileInfo,
+      histories,
+    } = yield call(getParams);
 
     yield call(
       isAvailableAction,
@@ -486,11 +495,11 @@ export function* deleteCommentWorker({
 
     if (answerId === 0) {
       questionData.comments = questionData.comments.filter(
-        (x) => x.id !== commentId,
+        x => x.id !== commentId,
       );
     } else if (answerId > 0) {
-      const answer = questionData.answers.find((x) => x.id === answerId);
-      answer.comments = answer.comments.filter((x) => x.id !== commentId);
+      const answer = questionData.answers.find(x => x.id === answerId);
+      answer.comments = answer.comments.filter(x => x.id !== commentId);
     }
 
     const newHistory = {
@@ -512,8 +521,13 @@ export function* deleteCommentWorker({
 
 export function* deleteAnswerWorker({ questionId, answerId, buttonId }) {
   try {
-    const { questionData, ethereumService, locale, profileInfo, histories } =
-      yield call(getParams);
+    const {
+      questionData,
+      ethereumService,
+      locale,
+      profileInfo,
+      histories,
+    } = yield call(getParams);
 
     yield call(
       isAvailableAction,
@@ -548,9 +562,7 @@ export function* deleteAnswerWorker({ questionId, answerId, buttonId }) {
 
     histories.push(newHistory);
 
-    questionData.answers = questionData.answers.filter(
-      (x) => x.id !== answerId,
-    );
+    questionData.answers = questionData.answers.filter(x => x.id !== answerId);
 
     saveChangedItemIdToSessionStorage(CHANGED_POSTS_KEY, questionId);
 
@@ -626,7 +638,7 @@ export function* getQuestionDataWorker({ questionId }) {
 
     if (account === questionData.author.id) {
       yield all(
-        answers.map(function* ({ author: answerUserInfo }) {
+        answers.map(function*({ author: answerUserInfo }) {
           const answerProfileInfo = yield select(selectUsers(author.id));
           if (!answerProfileInfo.profile) {
             const profile = JSON.parse(
@@ -708,8 +720,12 @@ export function* postCommentWorker({
   buttonId,
 }) {
   try {
-    const { questionData, ethereumService, profileInfo, histories } =
-      yield call(getParams);
+    const {
+      questionData,
+      ethereumService,
+      profileInfo,
+      histories,
+    } = yield call(getParams);
 
     yield call(checkPostCommentAvailableWorker, buttonId, answerId);
     const commentData = {
@@ -751,9 +767,9 @@ export function* postCommentWorker({
       });
     } else {
       const { comments, commentCount } = questionData.answers.find(
-        (x) => x.id === answerId,
+        x => x.id === answerId,
       );
-      questionData.answers.find((x) => x.id === answerId).commentCount += 1;
+      questionData.answers.find(x => x.id === answerId).commentCount += 1;
       commentId = commentCount + 1;
       comments.push({
         ...newComment,
@@ -787,8 +803,14 @@ export function* postCommentWorker({
 
 export function* postAnswerWorker({ questionId, answer, official, reset }) {
   try {
-    const { questionData, ethereumService, profileInfo, locale, histories } =
-      yield call(getParams);
+    const {
+      questionData,
+      ethereumService,
+      profileInfo,
+      locale,
+      histories,
+      account,
+    } = yield call(getParams);
 
     yield call(isAuthorized);
 
@@ -868,6 +890,11 @@ export function* postAnswerWorker({ questionId, answer, official, reset }) {
 
     saveChangedItemIdToSessionStorage(CHANGED_POSTS_KEY, questionId);
 
+    const updatedQuestionData = yield call(getQuestionData, {
+      questionId,
+      user: account,
+    });
+    yield put(getQuestionDataSuccess(updatedQuestionData));
     yield put(postAnswerSuccess(questionData));
   } catch (err) {
     yield put(postAnswerErr(err));
@@ -916,7 +943,7 @@ export function* downVoteWorker({
     const item =
       answerId === 0
         ? questionData
-        : questionData.answers.find((x) => x.id === answerId);
+        : questionData.answers.find(x => x.id === answerId);
 
     if (item.votingStatus.isDownVoted) {
       item.rating += 1;
@@ -974,7 +1001,7 @@ export function* upVoteWorker({
     const item =
       answerId === 0
         ? questionData
-        : questionData.answers.find((x) => x.id === answerId);
+        : questionData.answers.find(x => x.id === answerId);
 
     if (item.votingStatus.isUpVoted) {
       item.rating -= 1;
@@ -1072,15 +1099,13 @@ export function* voteToDeleteWorker({
     if (!item.answerId && !item.commentId) {
       itemData = questionData;
     } else if (!item.answerId && item.commentId) {
-      itemData = questionData.comments.filter(
-        (x) => x.id === item.commentId,
-      )[0];
+      itemData = questionData.comments.filter(x => x.id === item.commentId)[0];
     } else if (item.answerId && !item.commentId) {
-      itemData = questionData.answers.filter((x) => x.id === item.answerId)[0];
+      itemData = questionData.answers.filter(x => x.id === item.answerId)[0];
     } else if (item.answerId && item.commentId) {
       itemData = questionData.answers
-        .filter((x) => x.id === item.answerId)[0]
-        .comments.filter((y) => y.id === item.commentId)[0];
+        .filter(x => x.id === item.answerId)[0]
+        .comments.filter(y => y.id === item.commentId)[0];
     }
 
     yield call(
@@ -1123,11 +1148,11 @@ export function* voteToDeleteWorker({
         // delete comment
         if (answerId === 0) {
           questionData.comments = questionData.comments.filter(
-            (x) => x.id !== commentId,
+            x => x.id !== commentId,
           );
         } else if (answerId > 0) {
-          const answer = questionData.answers.find((x) => x.id === answerId);
-          answer.comments = answer.comments.filter((x) => x.id !== commentId);
+          const answer = questionData.answers.find(x => x.id === answerId);
+          answer.comments = answer.comments.filter(x => x.id !== commentId);
         }
 
         yield put(deleteCommentSuccess({ ...questionData }, buttonId));
@@ -1136,7 +1161,7 @@ export function* voteToDeleteWorker({
       if (isDeleteAnswerButton) {
         // delete answer
         questionData.answers = questionData.answers.filter(
-          (x) => x.id !== answerId,
+          x => x.id !== answerId,
         );
 
         yield put(deleteAnswerSuccess({ ...questionData }, buttonId));
@@ -1161,13 +1186,13 @@ export function* voteToDeleteWorker({
       if (!answerId && !commentId) {
         item = questionData;
       } else if (!answerId && commentId) {
-        item = questionData.comments.find((x) => x.id === commentId);
+        item = questionData.comments.find(x => x.id === commentId);
       } else if (answerId && !commentId) {
-        item = questionData.answers.find((x) => x.id === answerId);
+        item = questionData.answers.find(x => x.id === answerId);
       } else if (answerId && commentId) {
         item = questionData.answers
-          .find((x) => x.id === answerId)
-          .comments.find((x) => x.id === commentId);
+          .find(x => x.id === answerId)
+          .comments.find(x => x.id === commentId);
       }
 
       item.votingStatus.isVotedToDelete = true;
@@ -1205,7 +1230,7 @@ export function* updateQuestionDataAfterTransactionWorker({
 
     const userInfoMe = yield call(getUserProfileWorker, { user });
 
-    const changeUserInfo = (item) => {
+    const changeUserInfo = item => {
       if (item.author.user === user) {
         item.author = userInfoMe;
       } else if (item.author.user === usersForUpdate[0]) {
@@ -1214,11 +1239,11 @@ export function* updateQuestionDataAfterTransactionWorker({
     };
 
     changeUserInfo(questionData);
-    questionData.comments.forEach((x) => changeUserInfo(x));
+    questionData.comments.forEach(x => changeUserInfo(x));
 
-    questionData.answers.forEach((x) => {
+    questionData.answers.forEach(x => {
       changeUserInfo(x);
-      x.comments.forEach((y) => changeUserInfo(y));
+      x.comments.forEach(y => changeUserInfo(y));
     });
 
     yield put(getQuestionDataSuccess({ ...questionData }));
@@ -1285,7 +1310,7 @@ export function* updateQuestionList({ questionData }) {
   }
 }
 
-export default function* () {
+export default function*() {
   yield takeEvery(GET_QUESTION_DATA, getQuestionDataWorker);
   yield takeLatest(POST_ANSWER, postAnswerWorker);
   yield takeEvery(CHECK_ADD_COMMENT_AVAILABLE, showAddCommentFormWorker);
