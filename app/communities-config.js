f2ba21e82e3c0994eea37f647f1f56549e474f79
YuriyLu <yura2000uraura@gmail.com>
2022-11-13 13:39:53 +0100

    Revert local singlecommunity

--- app/communities-config.js
+++ app/communities-config.js
@@ -199,7 +199,7 @@ const communitiesConfig = {
   },
   dev: {
     1: {
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:31000',
       src: AaveLogo,
       styles: AaveStyles,
     },
