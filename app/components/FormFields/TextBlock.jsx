be91619bceaea06ece767892c8d11cb76bf7ee18
alexeishulga <alexeishulg@gmail.com>
2022-11-23 18:08:31 +0300

    display lists

--- app/components/FormFields/TextBlock.jsx
+++ app/components/FormFields/TextBlock.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import '@uiw/react-markdown-preview/markdown.css';
 
-export const TextBlock = ({ content }) => <MarkdownPreview source={content} />;
+export const TextBlock = ({ content }) => (
+  <MarkdownPreview
+    css={css`
+      ol li {
+        list-style-type: decimal;
+      }
+      ul li {
+        list-style-type: disc;
+      }
+    `}
+    source={content}
+    rehypeRewrite={(node) => {
+      if (node.tagName === 'input') {
+        node.properties.disabled = false;
+      }
+    }}
+  />
+);
 
 TextBlock.propTypes = {
   content: PropTypes.string,
