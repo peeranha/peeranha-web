39e1786c796445e32be0bd01c200b69051b96604
alexeishulga <alexeishulg@gmail.com>
2022-11-23 16:33:22 +0300

    add metaPeeranha logo

--- app/containers/Header/View.jsx
+++ app/containers/Header/View.jsx
@@ -18,6 +18,7 @@ import addIcon from 'images/add.svg?external';
 import searchIcon from 'images/search.svg?external';
 import headerNavigationIcon from 'images/headerNavigation.svg?external';
 import peeranhaLogo from 'images/LogoBlack.svg?inline';
+import peeranhaMetaLogo from 'images/PeeranhaMeta.svg?inline';
 
 import {
   isSingleCommunityWebsite,
@@ -184,10 +185,8 @@ const View = ({
 
   const Logo = useCallback(() => {
     if (isSearchFormVisible) return null;
-
-    const src = styles.withoutSubHeader
-      ? communitiesConfig[single].src
-      : peeranhaLogo;
+    const logo = single ? peeranhaMetaLogo : peeranhaLogo;
+    const src = styles.withoutSubHeader ? communitiesConfig[single].src : logo;
 
     return (
       <LogoStyles to={single ? routes.feed() : routes.home()}>
