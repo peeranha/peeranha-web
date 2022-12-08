53112c6ca478f722a9dc77b5cac67940d0bd09ec
aweawe95 <vitalikarach95@gmail.com>
2022-12-08 23:46:40 +0300

    fix tips in new community creation

--- app/components/Base/TipsBase.js
+++ app/components/Base/TipsBase.js
@@ -10,23 +10,23 @@ export default BaseRoundedNoPadding.extend`
     max-width: 100%;
   }
 
-  @media only screen and (min-width: 1280px) {
+  @media only screen and (min-width: 1366px) {
     > *:nth-child(1) {
-      flex: 0 0 calc(100% - 340px);
-      max-width: calc(100% - 340px);
+      flex: 0 0 calc(100% - 300px);
+      max-width: calc(100% - 300px);
     }
 
     > *:nth-child(2) {
-      flex: 0 0 340px;
-      max-width: 340px;
-      padding: 47px 36px;
+      flex: 0 0 300px;
+      max-width: 300px;
+      padding: 47px 31px;
       background: ${SECONDARY_SPECIAL_3};
       border-top-right-radius: ${BORDER_RADIUS_L};
       border-bottom-right-radius: ${BORDER_RADIUS_L};
     }
   }
 
-  @media only screen and (max-width: 1280px) {
+  @media only screen and (max-width: 1365px) {
     align-items: baseline;
     > *:nth-child(2) {
       display: none;
