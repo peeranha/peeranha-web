a4f5dc3242afdba647cd88c54441bc8812f3f8dd
aweawe95 <vitalikarach95@gmail.com>
2022-12-09 11:50:31 +0300

    Add protocol admin rights to edit documentation

--- app/containers/LeftMenu/MainLinks.jsx
+++ app/containers/LeftMenu/MainLinks.jsx
@@ -173,7 +173,8 @@ const MainLinks = ({
   const isProtocolAdmin = hasProtocolAdminRole(getPermissions(profile));
   const isModeratorModeSingleCommunity = singleCommId
     ? hasCommunityAdminRole(getPermissions(profile), singleCommId) ||
-      hasCommunityModeratorRole(getPermissions(profile), singleCommId)
+      hasCommunityModeratorRole(getPermissions(profile), singleCommId) ||
+      isProtocolAdmin
     : false;
 
   if (!route) {
