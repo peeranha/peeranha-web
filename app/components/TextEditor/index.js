7ea7eccf731abbfa24610fc735d272aa69d75e36
alexeishulga <alexeishulg@gmail.com>
2022-12-22 19:58:09 +0300

    fix table in MD

--- app/components/TextEditor/index.js
+++ app/components/TextEditor/index.js
@@ -78,6 +78,9 @@ class TextEditor extends React.PureComponent {
                   ul li {
                     list-style-type: disc;
                   }
+                  table {
+                    word-break: normal;
+                  }
                 `}
                 rehypeRewrite={(node) => {
                   if (node.tagName === 'input') {
