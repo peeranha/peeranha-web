638590b0a57f2e8a66823885b469b2b851c685ad
YuriyLu <yura2000uraura@gmail.com>
2022-12-22 11:03:49 +0100

    Fix border-radius, favicon

--- app/communities-config.js
+++ app/communities-config.js
@@ -264,7 +264,9 @@ const communitiesConfig = {
   },
   dev: {
     1: {
-      origin: 'http://localhost:31000',
+      origin: 'http://localhost:3000',
+      src: AnkrLogo,
+      styles: AnkrStyles,
     },
   },
 };
