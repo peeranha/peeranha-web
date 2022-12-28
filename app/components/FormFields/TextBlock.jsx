9841e980b0d1879774e5008eaf09d763b46e1d0c
alexeishulga <alexeishulg@gmail.com>
2022-12-28 15:37:19 +0300

    add youtube video

--- app/components/FormFields/TextBlock.jsx
+++ app/components/FormFields/TextBlock.jsx
@@ -15,12 +15,34 @@ export const TextBlock = ({ content }) => (
       ul li {
         list-style-type: disc;
       }
+      iframe {
+        max-width: 100%;
+      }
       min-width: 0;
     `}
     warpperElement={{ 'data-color-mode': 'light' }}
-    rehypeRewrite={(node) => {
+    rehypeRewrite={(node, index, parent) => {
       if (node.tagName === 'input') {
         node.properties.disabled = false;
+      }
+      if (
+        parent?.tagName === 'a' &&
+        parent?.properties.href.slice(0, 24) == 'https://www.youtube.com/'
+      ) {
+        const youtubeLink = `https://www.youtube.com/embed/${node.value.slice(
+          32,
+        )}`;
+        node.value = (
+          <iframe
+            width="560"
+            height="315"
+            src={youtubeLink}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen="true"
+          ></iframe>
+        );
       }
     }}
   />
