0070999623becf3560912f0114eede93e4ad27f1
aweawe95 <vitalikarach95@gmail.com>
2023-02-02 20:20:56 +0300

    add developerDAO community

--- app/communities-config.js
+++ app/communities-config.js
@@ -42,6 +42,7 @@ import { Mark3dStyles } from './communities-configs/mark3d';
 import { OrbisStyles } from './communities-configs/orbis';
 import { FractalVisionsStyles } from './communities-configs/fractalVisions';
 import { MoveStyles } from './communities-configs/move';
+import { DeveloperDAOStyles } from './communities-configs/developerDAO';
 
 const communitiesConfig = {
   prod: {
@@ -236,6 +237,10 @@ const communitiesConfig = {
       origin: `https://fractalvisions${process.env.COOKIE_DOMAIN}`,
       src: FractalVisionsLogo,
       styles: FractalVisionsStyles,
+    },
+    13: {
+      src: 'https://images.peeranha.io/communities/communityDAO/dao-login.svg',
+      styles: DeveloperDAOStyles,
     },
     // 1: {
     //   origin: `https://meta${process.env.COOKIE_DOMAIN}`,
