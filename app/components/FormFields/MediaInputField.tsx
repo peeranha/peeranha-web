7ab03d6ca8305d0ca396cbf40d44f402d6a39e4e
aweawe95 <vitalikarach95@gmail.com>
2023-02-16 13:00:36 +0300

    fix media config

--- app/components/FormFields/MediaInputField.tsx
+++ app/components/FormFields/MediaInputField.tsx
@@ -26,7 +26,7 @@ export const MediaInputField = ({
 }: MediaInputFieldProps) => {
   const config = [
     {
-      types: ['image'],
+      types: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'],
       maxFileSizeInBytes: 5 * 1000 * 1000,
       uploadFile: uploadImagesToIpfs,
       saveUploadedFileLink: setMediaLink,
