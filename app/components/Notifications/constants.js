eaca02648288e89e53bef8cd6c6fd1b41404db36
Gleb Klimov <gleb.klimov@fortegrp.com>
2023-03-09 11:21:23 +0100

    BLOC-42 Refactor Notification component

--- app/components/Notifications/constants.js
+++ app/components/Notifications/constants.js
@@ -1,3 +1,7 @@
+import * as routes from 'routes-config';
+
+import { POST_TYPE } from 'utils/constants';
+
 import answerIcon from 'images/answer.svg?external';
 import questionIcon from 'images/question2.svg?external';
 import bestAnswerIcon from 'images/bestAnswer.svg?external';
@@ -12,43 +16,29 @@ export const VERTICAL_OFFSET = 5 * ROW_HEIGHT;
 export const NOTIFICATIONS_REQUEST_LIMIT = 50;
 export const MARK_AS_READ_DELAY = 1000;
 
-export const MARK_ALL_NOTIFICATIONS_AS_READ =
-  'app/Notifications/MARK_ALL_NOTIFICATIONS_AS_READ';
-export const MARK_ALL_NOTIFICATIONS_AS_READ_SUCCESS =
-  'app/Notifications/MARK_ALL_NOTIFICATIONS_AS_READ_SUCCESS';
-export const MARK_ALL_NOTIFICATIONS_AS_READ_ERROR =
-  'app/Notifications/MARK_ALL_NOTIFICATIONS_AS_READ_ERROR';
+export const MARK_ALL_NOTIFICATIONS_AS_READ = 'app/Notifications/MARK_ALL_NOTIFICATIONS_AS_READ';
+export const MARK_ALL_NOTIFICATIONS_AS_READ_SUCCESS = 'app/Notifications/MARK_ALL_NOTIFICATIONS_AS_READ_SUCCESS';
+export const MARK_ALL_NOTIFICATIONS_AS_READ_ERROR = 'app/Notifications/MARK_ALL_NOTIFICATIONS_AS_READ_ERROR';
 
-export const LOAD_MORE_UNREAD_NOTIFICATIONS =
-  'app/Notifications/LOAD_MORE_UNREAD_NOTIFICATIONS';
-export const LOAD_MORE_UNREAD_NOTIFICATIONS_SUCCESS =
-  'app/Notifications/LOAD_MORE_UNREAD_NOTIFICATIONS_SUCCESS';
-export const LOAD_MORE_UNREAD_NOTIFICATIONS_ERROR =
-  'app/Notifications/LOAD_MORE_UNREAD_NOTIFICATIONS_ERROR';
+export const LOAD_MORE_UNREAD_NOTIFICATIONS = 'app/Notifications/LOAD_MORE_UNREAD_NOTIFICATIONS';
+export const LOAD_MORE_UNREAD_NOTIFICATIONS_SUCCESS = 'app/Notifications/LOAD_MORE_UNREAD_NOTIFICATIONS_SUCCESS';
+export const LOAD_MORE_UNREAD_NOTIFICATIONS_ERROR = 'app/Notifications/LOAD_MORE_UNREAD_NOTIFICATIONS_ERROR';
 
-export const LOAD_MORE_NOTIFICATIONS =
-  'app/Notifications/LOAD_MORE_NOTIFICATIONS';
-export const LOAD_MORE_NOTIFICATIONS_SUCCESS =
-  'app/Notifications/LOAD_MORE_NOTIFICATIONS_SUCCESS';
-export const LOAD_MORE_NOTIFICATIONS_ERROR =
-  'app/Notifications/LOAD_MORE_NOTIFICATIONS_ERROR';
+export const LOAD_MORE_NOTIFICATIONS = 'app/Notifications/LOAD_MORE_NOTIFICATIONS';
+export const LOAD_MORE_NOTIFICATIONS_SUCCESS = 'app/Notifications/LOAD_MORE_NOTIFICATIONS_SUCCESS';
+export const LOAD_MORE_NOTIFICATIONS_ERROR = 'app/Notifications/LOAD_MORE_NOTIFICATIONS_ERROR';
 
-export const MARK_AS_READ_NOTIFICATIONS_ALL =
-  'app/Notifications/MARK_AS_READ_NOTIFICATIONS_ALL';
-export const MARK_AS_READ_NOTIFICATIONS_UNREAD =
-  'app/Notifications/SET_READ_NOTIFICATIONS_UNREAD';
+export const MARK_AS_READ_NOTIFICATIONS_ALL = 'app/Notifications/MARK_AS_READ_NOTIFICATIONS_ALL';
+export const MARK_AS_READ_NOTIFICATIONS_UNREAD = 'app/Notifications/SET_READ_NOTIFICATIONS_UNREAD';
 
 export const MARK_AS_READ_SUCCESS = 'app/Notifications/MARK_AS_READ_SUCCESS';
 export const MARK_AS_READ_ERROR = 'app/Notifications/MARK_AS_READ_ERROR';
 
-export const CLEAR_NOTIFICATIONS_DATA =
-  'app/Notifications/CLEAR_NOTIFICATIONS_DATA';
+export const CLEAR_NOTIFICATIONS_DATA = 'app/Notifications/CLEAR_NOTIFICATIONS_DATA';
 
-export const SET_NOTIFICATIONS_INFO =
-  'app/Notifications/SET_NOTIFICATIONS_INFO';
+export const SET_NOTIFICATIONS_INFO = 'app/Notifications/SET_NOTIFICATIONS_INFO';
 
-export const FILTER_READ_TIMESTAMPS =
-  'app/Notifications/FILTER_UNREAD_TIMESTAMPS';
+export const FILTER_READ_TIMESTAMPS = 'app/Notifications/FILTER_UNREAD_TIMESTAMPS';
 
 export const SET_LAST_USER = 'app/Notifications/SET_LAST_USER';
 
@@ -126,4 +116,10 @@ export const NOTIFICATIONS_DATA = {
     keyTranslate: 'notifications.yourAnswerWasTipped',
     src: coinsIcon,
   },
+};
+
+export const ROUTES_BY_TYPE = {
+  [POST_TYPE.expertPost]: routes.expertPostView,
+  [POST_TYPE.generalPost]: routes.questionView,
+  [POST_TYPE.tutorial]: routes.tutorialView,
 };
