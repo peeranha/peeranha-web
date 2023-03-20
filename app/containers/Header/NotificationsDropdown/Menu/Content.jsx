98a5d39534cf5db7eb3dd025f9ba49ac7e969594
Gleb Klimov <gleb.klimov@fortegrp.com>
2023-03-20 12:15:19 +0100

    PEER-682 Add notifications for type and community changes

--- app/containers/Header/NotificationsDropdown/Menu/Content.jsx
+++ app/containers/Header/NotificationsDropdown/Menu/Content.jsx
@@ -17,15 +17,20 @@ import injectReducer from 'utils/injectReducer';
 import { rangeUnionWithIntersection } from 'utils/rangeOperations';
 
 import bellIcon from 'images/Notifications_Disabled.svg?external';
+
+import { selectCommunities } from 'containers/DataCacheProvider/selectors';
+
 import { IconXl } from 'components/Icon/IconWithSizes';
-
 import saga from 'components/Notifications/saga';
-
 import reducer from 'components/Notifications/reducer';
-
-import { selectReadNotificationsUnread, selectUnreadNotificationsLoading } from 'components/Notifications/selectors';
-
-import { loadMoreUnreadNotifications, markAsReadNotificationsUnread } from 'components/Notifications/actions';
+import {
+  selectReadNotificationsUnread,
+  selectUnreadNotificationsLoading,
+} from 'components/Notifications/selectors';
+import {
+  loadMoreUnreadNotifications,
+  markAsReadNotificationsUnread,
+} from 'components/Notifications/actions';
 
 import Notification from './Notification';
 
@@ -57,6 +62,7 @@ const Content = ({
   rowHeight,
   loading,
   notifications,
+  communities,
   readNotifications,
   markAsReadNotificationsUnreadDispatch,
   loadMoreUnreadNotificationsDispatch,
@@ -125,6 +131,7 @@ const Content = ({
       key={key}
       top={top}
       height={rowHeight}
+      communities={communities}
       notificationsNumber={notifications.length}
       paddingHorizontal="15"
       {...notifications[index]}
@@ -183,10 +190,17 @@ export default memo(
       (state) => ({
         loading: selectUnreadNotificationsLoading()(state),
         readNotifications: selectReadNotificationsUnread()(state),
+        communities: selectCommunities()(state),
       }),
       (dispatch) => ({
-        markAsReadNotificationsUnreadDispatch: bindActionCreators(markAsReadNotificationsUnread, dispatch),
-        loadMoreUnreadNotificationsDispatch: bindActionCreators(loadMoreUnreadNotifications, dispatch),
+        markAsReadNotificationsUnreadDispatch: bindActionCreators(
+          markAsReadNotificationsUnread,
+          dispatch,
+        ),
+        loadMoreUnreadNotificationsDispatch: bindActionCreators(
+          loadMoreUnreadNotifications,
+          dispatch,
+        ),
       }),
     ),
   )(Content),
