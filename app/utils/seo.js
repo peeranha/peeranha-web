5494230ea1a70c5b2f1f8981934ae7f7346ddd64
Dmitrijg24k <dmitrij3e5g@gmail.com>
2023-04-13 12:24:22 +0300

    feature/BLOC-80 : remove all punctuation from url

--- app/utils/seo.js
+++ app/utils/seo.js
@@ -1,5 +1,12 @@
 const updateSeoUrl = (title) =>
-  encodeURIComponent((title || '').trim().replace(/ /g, '-').toLowerCase());
+  encodeURIComponent(
+    (title || '')
+      .trim()
+      .replace(/[.,'#!$%^&*;:{}=\-_`~() ?]/g, '-')
+      .toLowerCase(),
+  )
+    .replace(/^-+|-+$/g, '')
+    .replace(/--/g, '-');
 
 export const updateTitle = (title) => {
   if (title === ':title') {
