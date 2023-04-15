5c4f79a10b2e583d648be235fb29e451da8cd9fe
YuriyLu <yura2000uraura@gmail.com>
2023-04-15 20:54:34 +0200

    Reafctoring

--- app/containers/ChangeLocale/ChangeLocaleButton.tsx
+++ app/containers/ChangeLocale/ChangeLocaleButton.tsx
@@ -24,13 +24,7 @@ const ChangeLocaleButton: React.FC<ChangeLocaleButtonProps> = ({
       fontSize="16"
       lineHeight="20"
       color={colors.commHeadElemColor || 'var(--color-gray-dark)'}
-      onClick={
-        typeof setOpen !== 'undefined'
-          ? () => {
-              setOpen(true);
-            }
-          : 'undefined'
-      }
+      onClick={() => setOpen?.(true)}
     >
       <Flag src={`https://images.peeranha.io/languages/${locale}_lang.svg`} alt="country" />
       {withTitle ? t(`common.${locale}`) : locale.toLocaleUpperCase()}
