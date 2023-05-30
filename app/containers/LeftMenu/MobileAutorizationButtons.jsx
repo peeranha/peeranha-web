39b6912c7c6484b49ddc882558e00cdc7c6ef997
YuriyLu <yura2000uraura@gmail.com>
2023-05-30 14:18:09 +0200

    Fix mobile login

--- app/containers/LeftMenu/MobileAutorizationButtons.jsx
+++ app/containers/LeftMenu/MobileAutorizationButtons.jsx
@@ -1,25 +1,24 @@
+import SuiConnectModals from 'components/SuiConnectModals';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import LargeOutlinedButton from 'components/Button/Outlined/InfoLarge';
 
-const MobileAutorizationButtons = ({
-  profile,
-  isMenuVisible,
-  showLoginModal,
-}) => {
+const MobileAutorizationButtons = ({ profile, isMenuVisible, showLoginModal }) => {
   const { t } = useTranslation();
 
   if (profile || !isMenuVisible) {
     return null;
   }
 
+  const buttonWithLogin = () => (
+    <LargeOutlinedButton className="d-flex align-items-center" onClick={showLoginModal}>
+      {t('common.login')}
+    </LargeOutlinedButton>
+  );
+
   return (
-    <div className="d-flex align-items-center">
-      <LargeOutlinedButton onClick={showLoginModal}>
-        {t('common.login')}
-      </LargeOutlinedButton>
-    </div>
+    <SuiConnectModals loginWithWallet={showLoginModal} actionButtonWithLogin={buttonWithLogin} />
   );
 };
 
