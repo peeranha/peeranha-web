562e4169f62f154ad630893632adca3a51ed1d73
Dmitrijg24k <dmitrij3e5g@gmail.com>
2023-07-20 17:35:23 +0400

    feature/BLOC-268-2: fix font size

--- app/components/FreeTrialBanner/FreeTrialBanner.tsx
+++ app/components/FreeTrialBanner/FreeTrialBanner.tsx
@@ -12,7 +12,7 @@ const FreeTrialBanner = (): JSX.Element => {
       <h3 css={styles.h3}>{t('common.bannerText')}</h3>
       <img css={styles.img} src={ideaBox} />
       <a
-        className="df jcc aic fz14"
+        className="df jcc aic fz16"
         href="https://docs.google.com/forms/d/1CUJdcntRUtavuURb0fcvjakfhVDYl6Q5nSjESth_Evo/edit"
         target="_blank"
         css={styles.button}
