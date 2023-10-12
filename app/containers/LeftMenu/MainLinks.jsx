37c42a541ce86b9fec1d061c5fb4f51953fb8106
Dmitrijg24k <dmitrij3e5g@gmail.com>
2023-10-12 11:37:13 +0400

    feature/BLOC-268: remove FreeTrialBanner

--- app/containers/LeftMenu/MainLinks.jsx
+++ app/containers/LeftMenu/MainLinks.jsx
@@ -330,7 +330,7 @@ const MainLinks = ({
         <div css={styles.dividerLinks} />
       </div>
 
-      <FreeTrialBanner isMenuVisible={isMenuVisible}></FreeTrialBanner>
+      {/* <FreeTrialBanner isMenuVisible={isMenuVisible}></FreeTrialBanner> */}
 
       <div css={styles.dividerLinks} />
 
