fafc0689c11adbef22dd5a26959fa5214418121d
Alexei <alexeishulg@gmail.com>
2024-03-13 15:48:17 +0300

    update GTM id

--- app/containers/ViewQuestion/saga.js
+++ app/containers/ViewQuestion/saga.js
@@ -323,7 +323,7 @@ export function* saveCommentWorker({
     yield put(saveCommentSuccess({ ...questionData }, buttonId, commentId));
     ReactGA.event({
       category: 'Users',
-      action: 'create_comment_copmleted',
+      action: 'create_comment_completed',
     });
   } catch (err) {
     if (isSuiBlockchain) {
