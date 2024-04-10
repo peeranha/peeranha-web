26c11041f814f3db05f9e895c67609e91d066206
Yury Lukashevich <57390905+YuriyLu@users.noreply.github.com>
2024-04-10 11:42:19 +0200

    BLOC-955 Switching to Index if TheGraph is behind (#1619)

--- app/communities-config.js
+++ app/communities-config.js
@@ -404,8 +404,8 @@ const communitiesConfig = {
     : {
         '1-22': {
           origin: 'http://localhost:3000',
-          src: 'https://images.peeranha.io/communities/aave/logo.svg',
-          styles: AaveStyles,
+          src: 'https://images.peeranha.io/communities/graph/logo.svg',
+          styles: GraphStyles,
         },
       },
 };
