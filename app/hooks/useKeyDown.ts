e6d0f1c13415d0417fc71a097c79bb1df6d7bfca
Alexei-Shulga <aleksey-peeranha@bloxroute.com>
2024-04-15 17:29:40 +0300

    add keyDown fn in mobile version

--- app/hooks/useKeyDown.ts
+++ app/hooks/useKeyDown.ts
@@ -3,7 +3,9 @@ import { useEffect, useCallback } from 'react';
 export default function useKeyDown(callback: () => void, keys: number[]) {
   const onKeyDown = useCallback(
     (event: KeyboardEvent) => {
-      const isKeyPressed = keys.some((key) => event.ctrlKey && event.keyCode === key);
+      const isKeyPressed = keys.some(
+        (key) => (event.ctrlKey || event.metaKey) && event.keyCode === key,
+      );
       if (isKeyPressed) {
         event.preventDefault();
         callback();
